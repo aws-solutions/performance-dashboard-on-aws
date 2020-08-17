78f662872c69328147c5e7130d8ee8f7238199bb
Fernando Dingler <fdingler@amazon.com>
2020-08-17 09:58:11 -0700

    Refactored CDK api definition
    cr https://code.amazon.com/reviews/CR-32012072

--- cdk/lib/backend-stack.ts
+++ cdk/lib/backend-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from '@aws-cdk/core';
-import * as apigateway from "@aws-cdk/aws-apigateway";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as iam from '@aws-cdk/aws-iam'; 
+import { BadgerApi } from './constructs/api';
 
 interface BackendStackProps extends cdk.StackProps {
     userPoolArn: string,
@@ -78,90 +78,15 @@ export class BackendStack extends cdk.Stack {
             ]
         }));
 
-        /**
-         * Badger API
-         */
-        const api = new apigateway.RestApi(this, "ApiGateway", {
-            description: "Badger API",
-            defaultCorsPreflightOptions: {
-                allowOrigins: apigateway.Cors.ALL_ORIGINS,
-                allowMethods: apigateway.Cors.ALL_METHODS,
-            }
-        });
-
-        const apiIntegration = new apigateway.LambdaIntegration(handler);
-        const authorizer = new apigateway.CfnAuthorizer(this, 'CognitoAuth', {
-            type: apigateway.AuthorizationType.COGNITO,
-            name: 'cognito-authorizer',
-            restApiId: api.restApiId,
-            providerArns: [props.userPoolArn],
-            identitySource: 'method.request.header.Authorization',
-        });
-        
-        const dashboard = api.root.addResource('dashboard');
-        dashboard.addMethod("GET", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        dashboard.addMethod("POST", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        const dashboardId = dashboard.addResource('{id}');
-        dashboardId.addMethod("GET", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        dashboardId.addMethod("PUT", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        dashboardId.addMethod("DELETE", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        const widget = dashboardId.addResource("widget");
-        widget.addMethod("POST", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        const topicarea = api.root.addResource('topicarea');
-        topicarea.addMethod("GET", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-        
-        topicarea.addMethod("POST", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        const topicareaId = topicarea.addResource('{id}');
-        topicareaId.addMethod("GET", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        topicareaId.addMethod("PUT", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
-        });
-
-        topicareaId.addMethod("DELETE", apiIntegration, {
-            authorizationType: apigateway.AuthorizationType.COGNITO,
-            authorizer: { authorizerId: authorizer.ref },
+        const badgerApi = new BadgerApi(this, "BadgerApi", {
+            cognitoUserPoolArn: props.userPoolArn,
+            apiFunction: handler,
         });
 
         /**
          * Outputs
          */
-        this.apiGatewayEndpoint = api.url;
+        this.apiGatewayEndpoint = badgerApi.api.url;
         this.dynamodbTableName = table.tableName;
         new cdk.CfnOutput(this, 'ApiGatewayEndpoint', { value: this.apiGatewayEndpoint });
         new cdk.CfnOutput(this, 'DynamoDbTableName', { value: this.dynamodbTableName });

6f0f9d014daa2c32208abeec0a31458d2924cf8f
Fernando Dingler <fdingler@amazon.com>
2020-09-28 16:45:55 -0700

    GTT-398: Backend to retrieve homepage

---
+++ backend/src/lib/factories/__tests__/homepage-factory.test.ts
@@ -0,0 +1,30 @@
+import { Homepage, HomepageItem } from "../../models/homepage";
+import HomepageFactory from "../homepage-factory";
+
+describe("getDefaultHomepage", () => {
+  it("returns default values", () => {
+    expect(HomepageFactory.getDefaultHomepage()).toEqual({
+      title: "Performance Dashboard",
+      description:
+        "The Performance Dashboard makes data open " +
+        "and accessible to provide transparency and help drive the " +
+        "ongoing improvement of digital services.",
+    });
+  });
+});
+
+describe("fromItem", () => {
+  it("converts a dynamodb item to a Homepage object", () => {
+    const item: HomepageItem = {
+      pk: "Homepage",
+      sk: "Homepage",
+      type: "Homepage",
+      title: "Kingdom of Wakanda",
+      description: "Welcome to Wakanda",
+    };
+
+    const homepage = HomepageFactory.fromItem(item);
+    expect(homepage.title).toEqual("Kingdom of Wakanda");
+    expect(homepage.description).toEqual("Welcome to Wakanda");
+  });
+});

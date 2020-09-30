e58841005f5acda9a7091bcd1c0b78bc4b96c47a
Fernando Dingler <fdingler@amazon.com>
2020-09-30 09:19:37 -0700

    GTT-400: Published dashboards in homepage api

--- backend/src/lib/controllers/__tests__/homepage-ctrl.test.ts
+++ backend/src/lib/controllers/__tests__/homepage-ctrl.test.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from "express";
 import { mocked } from "ts-jest/utils";
+import {
+  Dashboard,
+  DashboardState,
+  PublicDashboard,
+} from "../../models/dashboard";
 import HomepageFactory from "../../factories/homepage-factory";
 import HomepageRepository from "../../repositories/homepage-repo";
+import DashboardRepository from "../../repositories/dashboard-repo";
+import DashboardFactory from "../../factories/dashboard-factory";
 import HomepageCtrl from "../homepage-ctrl";
 
 jest.mock("../../repositories/homepage-repo");
+jest.mock("../../repositories/dashboard-repo");
+jest.mock("../../factories/dashboard-factory");
 jest.mock("../../factories/homepage-factory");
 
 const repository = mocked(HomepageRepository.prototype);
+const dashboardRepo = mocked(DashboardRepository.prototype);
 const req = ({} as any) as Request;
 const res = ({
   send: jest.fn().mockReturnThis(),
@@ -17,6 +27,8 @@ const res = ({
 
 beforeEach(() => {
   HomepageRepository.getInstance = jest.fn().mockReturnValue(repository);
+  DashboardRepository.getInstance = jest.fn().mockReturnValue(dashboardRepo);
+  dashboardRepo.listPublishedDashboards = jest.fn().mockReturnValue([]);
 });
 
 describe("getHomepage", () => {
@@ -32,10 +44,12 @@ describe("getHomepage", () => {
 
     await HomepageCtrl.getHomepage(req, res);
     expect(HomepageFactory.getDefaultHomepage).toBeCalled();
-    expect(res.json).toBeCalledWith({
-      title: "Performance Dashboard",
-      description: "Welcome to the performance dashboard",
-    });
+    expect(res.json).toBeCalledWith(
+      expect.objectContaining({
+        title: "Performance Dashboard",
+        description: "Welcome to the performance dashboard",
+      })
+    );
   });
 
   it("returns homepage when available in the database", async () => {
@@ -47,9 +61,49 @@ describe("getHomepage", () => {
 
     await HomepageCtrl.getHomepage(req, res);
     expect(HomepageFactory.getDefaultHomepage).not.toBeCalled();
-    expect(res.json).toBeCalledWith({
-      title: "Kingdom of Wakanda",
-      description: "Welcome to the performance dashboard of our kingdom",
-    });
+    expect(res.json).toBeCalledWith(
+      expect.objectContaining({
+        title: "Kingdom of Wakanda",
+        description: "Welcome to the performance dashboard of our kingdom",
+      })
+    );
+  });
+
+  it("returns a list of published dashboards", async () => {
+    const now = new Date();
+    const published: Array<Dashboard> = [
+      {
+        id: "123",
+        name: "Banana",
+        description: "Something",
+        createdBy: "johndoe",
+        updatedAt: now,
+        state: DashboardState.Published,
+        topicAreaId: "xyz",
+        topicAreaName: "Health and Human Services",
+      },
+    ];
+
+    dashboardRepo.listPublishedDashboards = jest
+      .fn()
+      .mockReturnValue(published);
+
+    const publicDashboard: PublicDashboard = {
+      id: "123",
+      name: "Banana",
+      description: "Something",
+      updatedAt: now,
+      topicAreaId: "xyz",
+      topicAreaName: "Health and Human Services",
+    };
+
+    DashboardFactory.toPublic = jest.fn().mockReturnValue(publicDashboard);
+    await HomepageCtrl.getHomepage(req, res);
+
+    expect(res.json).toBeCalledWith(
+      expect.objectContaining({
+        dashboards: [publicDashboard],
+      })
+    );
   });
 });
--- backend/src/lib/controllers/homepage-ctrl.ts
+++ backend/src/lib/controllers/homepage-ctrl.ts
@@ -1,16 +1,25 @@
 import { Request, Response } from "express";
 import HomepageFactory from "../factories/homepage-factory";
 import HomepageRepository from "../repositories/homepage-repo";
+import DashboardRepository from "../repositories/dashboard-repo";
+import DashboardFactory from "../factories/dashboard-factory";
 
 async function getHomepage(req: Request, res: Response) {
   const repo = HomepageRepository.getInstance();
-  const homepage = await repo.getHomepage();
+  const dashboardRepo = DashboardRepository.getInstance();
+
+  let homepage = await repo.getHomepage();
+  const dashboards = await dashboardRepo.listPublishedDashboards();
+  const publicDashboards = dashboards.map(DashboardFactory.toPublic);
 
   if (!homepage) {
-    return res.json(HomepageFactory.getDefaultHomepage());
+    homepage = HomepageFactory.getDefaultHomepage();
   }
 
-  return res.json(homepage);
+  return res.json({
+    ...homepage,
+    dashboards: publicDashboards,
+  });
 }
 
 export default {

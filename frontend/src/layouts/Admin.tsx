c72dd4f7f4e9259af072160f5e344cfb796e451f
trietlu <trietlu@amazon.com>
2020-10-14 21:17:46 -0400

    Add Logout dropdown menu to the header used for Admin pages

--- frontend/src/layouts/Admin.tsx
+++ frontend/src/layouts/Admin.tsx
@@ -1,10 +1,25 @@
 import React, { ReactNode } from "react";
+import { Auth } from "aws-amplify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
+async function signOut(): Promise<boolean> {
+  try {
+    await Auth.signOut();
+    return true;
+  } catch (error) {
+    console.log("error signing out: ", error);
+    return false;
+  }
+}
+
 function AdminLayout(props: LayoutProps) {
+  //const history = useHistory();
+
   return (
     <>
       <div className="usa-overlay"></div>
@@ -18,7 +33,41 @@ function AdminLayout(props: LayoutProps) {
                 </a>
               </em>
             </div>
+            <button className="usa-menu-btn">Menu</button>
           </div>
+          <nav aria-label="Primary navigation" className="usa-nav">
+            <button className="usa-nav__close">
+              <FontAwesomeIcon icon={faWindowClose} size="lg" role="img" />
+            </button>
+            <ul className="usa-nav__primary usa-accordion">
+              <li className="usa-nav__primary-item">
+                <button
+                  className="usa-accordion__button usa-nav__link  usa-current"
+                  aria-expanded="false"
+                  aria-controls="basic-nav-section-one"
+                >
+                  <span>Admin</span>
+                </button>
+                <ul
+                  id="basic-nav-section-one"
+                  className="usa-nav__submenu"
+                  hidden
+                >
+                  <li className="usa-nav__submenu-item">
+                    <a
+                      href="/admin"
+                      onClick={async () => {
+                        await signOut();
+                      }}
+                      className="usa-link"
+                    >
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
       <main className="padding-y-3">

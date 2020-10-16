399eec0082fa0a0ab5de0da5ee4355ce149709c7
Triet <triet05@gmail.com>
2020-10-16 13:51:43 -0400

    Enable logout (#42)
    Add Logout dropdown menu to the header used for Admin pages.  Redirect user to /admin (login page) after logout

--- frontend/src/layouts/Admin.tsx
+++ frontend/src/layouts/Admin.tsx
@@ -1,7 +1,19 @@
 import React, { ReactNode } from "react";
+import { Auth } from "aws-amplify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 
 interface LayoutProps {
   children: ReactNode;
+}
+
+async function signOut(event: React.MouseEvent) {
+  try {
+    await Auth.signOut();
+  } catch (error) {
+    console.log("error signing out: ", error);
+    event.preventDefault();
+  }
 }
 
 function AdminLayout(props: LayoutProps) {
@@ -18,7 +30,35 @@ function AdminLayout(props: LayoutProps) {
                 </a>
               </em>
             </div>
+            <button className="usa-menu-btn">Menu</button>
           </div>
+          <nav aria-label="Primary navigation" className="usa-nav">
+            <button className="usa-nav__close">
+              <FontAwesomeIcon icon={faWindowClose} size="lg" role="img" />
+            </button>
+            <ul className="usa-nav__primary usa-accordion">
+              <li className="usa-nav__primary-item">
+                <button
+                  className="usa-accordion__button usa-nav__link  usa-current"
+                  aria-expanded="false"
+                  aria-controls="basic-nav-section-one"
+                >
+                  <span>Admin</span>
+                </button>
+                <ul
+                  id="basic-nav-section-one"
+                  className="usa-nav__submenu"
+                  hidden
+                >
+                  <li className="usa-nav__submenu-item">
+                    <a href="/admin" onClick={signOut} className="usa-link">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
       <main className="padding-y-3">

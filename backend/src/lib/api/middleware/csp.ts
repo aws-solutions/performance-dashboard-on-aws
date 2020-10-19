2c44dbe8549a41dbc3584b53fe3a1fe96a0d5fc5
Triet <triet05@gmail.com>
2020-10-19 11:28:41 -0400

    Return security http headers in web and API responses (#46)
    Co-authored-by: trietlu <trietlu@amazon.com>

---
+++ backend/src/lib/api/middleware/csp.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+
+/**
+ * Inject security related HTTP headers into the responses
+ */
+const csp = function(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; block-all-mixed-content;"
+  );
+  res.setHeader(
+    "Strict-Transport-Security",
+    "max-age=31540000; includeSubdomains"
+  );
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  next();
+};
+
+export default csp;

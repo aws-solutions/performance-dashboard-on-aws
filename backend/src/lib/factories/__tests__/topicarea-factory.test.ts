7cc4805f90f4525c529ca28ab9f073e6df150ad4
Fernando Dingler <fdingler@amazon.com>
2020-10-21 16:29:30 -0700

    Fixed backend files with prettier

--- backend/src/lib/factories/__tests__/topicarea-factory.test.ts
+++ backend/src/lib/factories/__tests__/topicarea-factory.test.ts
@@ -1,67 +1,67 @@
-import factory from '../topicarea-factory';
-import { TopicArea } from '../../models/topicarea';
-import { User } from '../../models/user';
+import factory from "../topicarea-factory";
+import { TopicArea } from "../../models/topicarea";
+import { User } from "../../models/user";
 
 const user: User = {
-  userId: 'johndoe',
+  userId: "johndoe",
 };
 
-describe('TopicAreaFactory.create', () => {
-  it('should create a new topic area with name', () => {
-    const topicarea = factory.create('123', 'AWS', user);
-    expect(topicarea.id).toEqual('123');
-    expect(topicarea.name).toEqual('AWS');
+describe("TopicAreaFactory.create", () => {
+  it("should create a new topic area with name", () => {
+    const topicarea = factory.create("123", "AWS", user);
+    expect(topicarea.id).toEqual("123");
+    expect(topicarea.name).toEqual("AWS");
   });
 
-  it('should create a new topic area with createdBy', () => {
-    const topicarea = factory.create('123', 'AWS', user);
+  it("should create a new topic area with createdBy", () => {
+    const topicarea = factory.create("123", "AWS", user);
     expect(topicarea.createdBy).toEqual(user.userId);
   });
 });
 
-describe('TopicAreaFactory.createNew', () => {
-  it('should create a new topic area with unique id', () => {
-    const topicarea1 = factory.createNew('Banana', user);
-    const topicarea2 = factory.createNew('Strawberry', user);
+describe("TopicAreaFactory.createNew", () => {
+  it("should create a new topic area with unique id", () => {
+    const topicarea1 = factory.createNew("Banana", user);
+    const topicarea2 = factory.createNew("Strawberry", user);
     expect(topicarea1.id).not.toEqual(topicarea2.id);
   });
 
-  it('should create a new topic area with name', () => {
-    const topicarea = factory.createNew('AWS', user);
-    expect(topicarea.name).toEqual('AWS');
+  it("should create a new topic area with name", () => {
+    const topicarea = factory.createNew("AWS", user);
+    expect(topicarea.name).toEqual("AWS");
   });
 
-  it('should create a new topic area with createdBy', () => {
-    const topicarea = factory.createNew('AWS', user);
+  it("should create a new topic area with createdBy", () => {
+    const topicarea = factory.createNew("AWS", user);
     expect(topicarea.createdBy).toEqual(user.userId);
   });
 });
 
-describe('TopicAreaFactory.toItem', () => {
-  const topicarea : TopicArea = {
-    id: '123',
-    name: 'Banana',
+describe("TopicAreaFactory.toItem", () => {
+  const topicarea: TopicArea = {
+    id: "123",
+    name: "Banana",
     createdBy: user.userId,
   };
 
-  it('should have a pk that starts with TopicArea', () => {
+  it("should have a pk that starts with TopicArea", () => {
     const item = factory.toItem(topicarea);
-    expect(item.pk).toEqual('TopicArea#123');
+    expect(item.pk).toEqual("TopicArea#123");
   });
 
-  it('should have an sk equal to pk', () => {
+  it("should have an sk equal to pk", () => {
     const item = factory.toItem(topicarea);
     expect(item.sk).toEqual(item.pk);
   });
 
-  it('should have a type attribute', () => {
+  it("should have a type attribute", () => {
     const item = factory.toItem(topicarea);
-    expect(item.type).toEqual('TopicArea');
+    expect(item.type).toEqual("TopicArea");
   });
 
-  it('should include all other attributes of topic area', () => {
+  it("should include all other attributes of topic area", () => {
     const item = factory.toItem(topicarea);
-    expect(item.name).toEqual('Banana');
+    expect(item.name).toEqual("Banana");
     expect(item.createdBy).toEqual(user.userId);
   });
 });

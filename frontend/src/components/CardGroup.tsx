f2f02e1fdf623de35752375e98c7902492ec3f59
Fernando Dingler <fdingler@amazon.com>
2020-10-22 15:49:10 -0700

    GTT-486 Frontend new admin home page

---
+++ frontend/src/components/CardGroup.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+interface CardGroupProps {
+  children: React.ReactNode;
+}
+
+function CardGroup(props: CardGroupProps) {
+  return (
+    <ul className="usa-card-group">
+      {React.Children.map(props.children, (child) => {
+        if ((child as React.ReactElement).type === Card) {
+          return child;
+        }
+      })}
+    </ul>
+  );
+}
+
+interface CardProps {
+  title: string;
+  col: number;
+  children: React.ReactNode;
+}
+
+function Card(props: CardProps) {
+  return (
+    <li className={`usa-card cursor-default tablet:grid-col-${props.col}`}>
+      <div className="usa-card__container">
+        <header className="usa-card__header">
+          <h2 className="usa-card__heading font-family-sans">{props.title}</h2>
+        </header>
+        {React.Children.map(props.children, (child) => {
+          if ((child as React.ReactElement).type === CardBody) {
+            return child;
+          }
+        })}
+        {React.Children.map(props.children, (child) => {
+          if ((child as React.ReactElement).type === CardFooter) {
+            return child;
+          }
+        })}
+      </div>
+    </li>
+  );
+}
+
+interface CardBodyProps {
+  children: React.ReactNode;
+}
+
+function CardBody(props: CardBodyProps) {
+  return <div className="usa-card__body">{props.children}</div>;
+}
+
+interface CardFooterProps {
+  children: React.ReactNode;
+}
+
+function CardFooter(props: CardFooterProps) {
+  return <div className="usa-card__footer">{props.children}</div>;
+}
+
+CardGroup.CardFooter = CardFooter;
+CardGroup.CardBody = CardBody;
+CardGroup.Card = Card;
+export default CardGroup;

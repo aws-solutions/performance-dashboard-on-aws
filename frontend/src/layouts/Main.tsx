a338854667578de90a6d64d3766b6dd7f4c7402c
Fernando Dingler <ferdingler@users.noreply.github.com>
2020-10-27 08:21:21 -0700

    GTT-511 Added contact button defined in .env file (#67)
    * GTT-511 Added contact button defined in .env file
    
    Co-authored-by: Miguel Pavon Diaz <71112226+miguelpdiaz8@users.noreply.github.com>

--- frontend/src/layouts/Main.tsx
+++ frontend/src/layouts/Main.tsx
@@ -1,4 +1,7 @@
 import React, { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import "./Main.css";
 
 interface LayoutProps {
   children?: ReactNode;
@@ -10,7 +13,7 @@ function MainLayout(props: LayoutProps) {
       <div className="usa-overlay"></div>
       <header className="usa-header usa-header--basic">
         <div className="usa-nav-container">
-          <div className="usa-navbar">
+          <div className="usa-navbar navbar-long">
             <div className="usa-logo" id="basic-logo">
               <em className="usa-logo__text">
                 <a href="/" title="Home" aria-label="Home">
@@ -18,7 +21,20 @@ function MainLayout(props: LayoutProps) {
                 </a>
               </em>
             </div>
+            <button className="usa-menu-btn">Menu</button>
           </div>
+          <nav aria-label="Primary navigation" className="usa-nav">
+            <button className="usa-nav__close">
+              <FontAwesomeIcon icon={faWindowClose} size="lg" role="img" />
+            </button>
+            <ul className="usa-nav__primary usa-accordion">
+              <li className="usa-nav__primary-item">
+                <a href={`mailto:${process.env.REACT_APP_CONTACT_EMAIL}`}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
       <main className="padding-y-3">

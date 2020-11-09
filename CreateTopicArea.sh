39c4adc88b2c33dfc69be96268f848948ab186fa
Habib <tariqha@38f9d3591e84.ant.amazon.com>
2020-11-09 00:51:19 -0500

    modified script to read all variable from user. None of the fields are hardcoded anymore.

--- CreateTopicArea.sh
+++ CreateTopicArea.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-# Replace the post_url in this Format-> https://cognito-idp.{region-code}.amazonaws.com/{userPool ID}
-post_url='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_LiNDVdtYT'
+#read and create api request source
+read -p 'Cognito region code: ' region
+read -p 'Cognito user pool ID: ' userPool
+post_url='https://cognito-idp.'"$region"'.amazonaws.com/'"$userPool"
 
-# Replace the ClientId with the App Client ID of the user pool
-# Replace USERNAME and PASSWORD accordingly
+# reads USERNAME PASSWORD and clientID 
+read -p 'App Client ID: ' clientID
+read -p 'Username: ' userName
+read -e -s -p "Password (hidden):" pass
+printf "\n"
+
+# create JSON payload for JWT token request
+generate_post_token()
+{
+  cat <<EOF
+    {
+      "ClientId": "$clientID",
+      "AuthFlow": "USER_PASSWORD_AUTH",
+      "AuthParameters": {
+          "USERNAME": "$userName",
+          "PASSWORD": "$pass"
+          }
+      }
+EOF
+}
+
 token_request=$(curl --location --request POST "$post_url" \
 --header 'Content-Type: application/x-amz-json-1.1' \
 --header 'X-Amz-Target: AWSCognitoIdentityProviderService.InitiateAuth' \
---data-raw '{
-                "ClientId": "515m3r2gggvng79osn9dg69cct",
-                "AuthFlow": "USER_PASSWORD_AUTH",
-                "AuthParameters": {
-                    "USERNAME": "tariqha@amazon.com",
-                    "PASSWORD": "Lalala123#"
-                    }
-            }')
+--data-raw "$(generate_post_token)")
 
+# extracting JWT token
 jwt_token=$(echo $token_request | jq --raw-output '.AuthenticationResult.IdToken')
 
+# reads topic name 
 read -p 'enter the topic name: ' topicName
-echo $topicName
 
+# JOSN payload for topic create request
 generate_post_data()
 {
   cat <<EOF

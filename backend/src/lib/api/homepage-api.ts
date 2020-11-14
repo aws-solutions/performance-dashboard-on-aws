0338902bdcc0370e60a05e16e2946d08c99a856e
Fernando Dingler <fdingler@amazon.com>
2020-11-13 16:51:13 -0800

    Refactor: Moved homepage api into public api

--- backend/src/lib/api/homepage-api.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import HomepageCtrl from "../controllers/homepage-ctrl";
-import withErrorHandler from "./middleware/error-handler";
-
-const router = Router();
-
-router.get("/", withErrorHandler(HomepageCtrl.getHomepage));
-
-export default router;

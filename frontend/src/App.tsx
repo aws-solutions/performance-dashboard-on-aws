939c4c37ec7c8599168093078a183c8195d5abab
Fernando Dingler <fdingler@amazon.com>
2020-12-11 14:42:16 -0800

    GTT-761 Added footer in 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import SettingsProvider from "./context/SettingsProvider";
 
@@ -191,11 +196,7 @@ function App() {
               />
             );
           })}
-          <Route
-            key={"/admin/"}
-            component={withAdminLayout(FourZeroFour)}
-            path={"/admin/"}
-          />
+          <Redirect from="*" to="/404/page-not-found" />
         </Switch>
       </Router>
     </SettingsProvider>

df66c538c86c8adf06c93df3efcc929994c5c8ee
Clark <clrknje@a483e778dcd5.ant.amazon.com>
2020-12-17 21:42:59 -0500

    Added API Help Page

---
+++ frontend/src/containers/__tests__/APIHelpPage.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import APIHelpPage from "../APIHelpPage";
+import { MemoryRouter } from "react-router-dom";
+
+test("renders the API Help Page component", async () => {
+  const wrapper = render(<APIHelpPage />, { wrapper: MemoryRouter });
+  expect(wrapper.container).toMatchSnapshot();
+});

a6545234f198901dec16d6aa5fb58199e1ad92e4
Andrew Mokotoff <amokotoff@gmail.com>
2021-01-05 23:15:19 -0500

    Added -optional label to the add/edit table screens

--- frontend/src/containers/AddTable.tsx
+++ frontend/src/containers/AddTable.tsx
@@ -451,7 +451,7 @@ function AddTable() {
                 <TextField
                   id="summary"
                   name="summary"
-                  label="Table summary"
+                  label="Table summary - optional"
                   hint="Give your table a summary to explain it in more depth.
                   It can also be read by screen readers to describe the table
                   for those with visual impairments."

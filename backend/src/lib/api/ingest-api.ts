2b3d697ba3e95730adfb05ff5dff671a54407abe
Fernando Dingler <fdingler@amazon.com>
2021-01-13 10:59:31 -0800

    GTT-841 Role-based access control in Backend

--- backend/src/lib/api/ingest-api.ts
+++ backend/src/lib/api/ingest-api.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import IngestApiCtrl from "../controllers/ingestapi-ctrl";
-import withErrorHandler from "./middleware/error-handler";
+import errorHandler from "./middleware/error-handler";
 
 const router = Router();
 
-router.post("/dataset", withErrorHandler(IngestApiCtrl.createDataset));
-router.put("/dataset/:id", withErrorHandler(IngestApiCtrl.updateDataset));
-router.delete("/dataset/:id", withErrorHandler(IngestApiCtrl.deleteDataset));
+router.post("/dataset", errorHandler(IngestApiCtrl.createDataset));
+router.put("/dataset/:id", errorHandler(IngestApiCtrl.updateDataset));
+router.delete("/dataset/:id", errorHandler(IngestApiCtrl.deleteDataset));
 
 export default router;
---
+++ backend/src/lib/api/middleware/rbac.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import { Role } from "../../models/user";
+
+/**
+ * Role-based access control (RBAC) middleware.
+ * Assumes that the `auth` middleware has already placed
+ * the user in the Request object.
+ */
+const rbac = (...allowedRoles: Role[]) =>
+  function (req: Request, res: Response, next: NextFunction) {
+    const user = req.user;
+    if (!user) {
+      // This technically should not happen. If the request got
+      // all the way to this middleware, the user should already
+      // be in the request object. But just in case ...
+      return res.status(401).send("Unauthorized");
+    }
+
+    if (!user.roles) {
+      return res
+        .status(403)
+        .send(`User ${user.userId} does not have a role assigned`);
+    }
+
+    // Get the intersection of the Allowed Roles and the user's roles.
+    // i.e. User has roles [A,B] and AllowedRoles are [B].
+    // Intersection should return B, hence the request should be allowed.
+    const intersection = user.roles.filter((role) =>
+      allowedRoles.includes(role)
+    );
+
+    if (intersection.length === 0) {
+      return res
+        .status(403)
+        .send(`User ${user.userId} is not authorized to access ${req.path}`);
+    }
+
+    next();
+  };
+
+export default rbac;

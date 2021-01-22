011f4e800a692b4c6e1c877d39f6cbbeada3806a
Andrew Mokotoff <amokotoff@gmail.com>
2021-01-22 16:37:47 -0500

    Cleaned up test

--- frontend/src/components/__tests__/ImagePreview.test.tsx
+++ frontend/src/components/__tests__/ImagePreview.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import ImagePreview from "../ImagePreview";
 

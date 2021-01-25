2b6455b721bc94d95b71b8718ca15c98d619433f
Andrew Mokotoff <amokotoff@gmail.com>
2021-01-25 17:17:27 -0500

    GTT-884: Updated all the dashboard screens to display image widgets

---
+++ frontend/src/hooks/image-hooks.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import StorageService from "../services/StorageService";
+
+type UseImageHook = {
+  loading: boolean;
+  file: File | undefined;
+};
+
+export function useImage(s3Key: string): UseImageHook {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [file, setFile] = useState<File>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const data = await StorageService.downloadFile(s3Key);
+      console.log(data);
+      setFile(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, [s3Key]);
+
+  return {
+    loading,
+    file,
+  };
+}

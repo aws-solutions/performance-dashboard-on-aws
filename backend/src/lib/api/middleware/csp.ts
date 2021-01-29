54ec166914ee079ee1f62f9d4f2bdf22c2d1ad32
Miguel Pavon Diaz <71112226+miguelpdiaz8@users.noreply.github.com>
2021-01-29 12:25:27 -0500

    [AppSec] Dataset Ingestion API - Set cache control in header (#262)
    * [AppSec] Dataset Ingestion API - Set cache control in header
    
    * [AppSec] Dataset Ingestion API - Set cache control in header
    
    * Fix tests
    
    * Fix conflicts
    
    * Add Cache-control header to Express
    
    Co-authored-by: Andrew Mokotoff <amokotoff@gmail.com>

--- backend/src/lib/api/middleware/csp.ts
+++ backend/src/lib/api/middleware/csp.ts
@@ -4,17 +4,15 @@ import { Request, Response, NextFunction } from "express";
  * Inject security related HTTP headers into the responses
  */
 const csp = function (req: Request, res: Response, next: NextFunction) {
-  res.setHeader(
+  res.set(
     "Content-Security-Policy",
     "default-src 'self'; block-all-mixed-content;"
   );
-  res.setHeader(
-    "Strict-Transport-Security",
-    "max-age=31540000; includeSubdomains"
-  );
-  res.setHeader("X-XSS-Protection", "1; mode=block");
-  res.setHeader("X-Frame-Options", "DENY");
-  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.set("Strict-Transport-Security", "max-age=31540000; includeSubdomains");
+  res.set("X-XSS-Protection", "1; mode=block");
+  res.set("X-Frame-Options", "DENY");
+  res.set("X-Content-Type-Options", "nosniff");
+  res.set("Cache-control", "public, max-age=600");
   next();
 };
 

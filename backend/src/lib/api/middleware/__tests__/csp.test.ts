c8d712253ea9deb78b9d27d39125a2114aeaba15
Miguel Pavon Diaz <migdizn@amazon.com>
2021-01-29 12:11:26 -0500

    Add Cache-control header to Express

--- backend/src/lib/api/middleware/__tests__/csp.test.ts
+++ backend/src/lib/api/middleware/__tests__/csp.test.ts
@@ -3,7 +3,7 @@ import csp from "../csp";
 
 const req = {} as Request;
 const res = {
-  setHeader: jest.fn() as any,
+  set: jest.fn() as any,
 } as Response;
 const next = jest.fn();
 
@@ -11,22 +11,20 @@ describe("csp", () => {
   it("injects csp headers", () => {
     csp(req, res, next);
 
-    expect(res.setHeader).toHaveBeenCalledWith(
+    expect(res.set).toHaveBeenCalledWith(
       "Content-Security-Policy",
       "default-src 'self'; block-all-mixed-content;"
     );
-    expect(res.setHeader).toHaveBeenCalledWith(
+    expect(res.set).toHaveBeenCalledWith(
       "Strict-Transport-Security",
       "max-age=31540000; includeSubdomains"
     );
-    expect(res.setHeader).toHaveBeenCalledWith(
-      "X-XSS-Protection",
-      "1; mode=block"
-    );
-    expect(res.setHeader).toHaveBeenCalledWith("X-Frame-Options", "DENY");
-    expect(res.setHeader).toHaveBeenCalledWith(
-      "X-Content-Type-Options",
-      "nosniff"
+    expect(res.set).toHaveBeenCalledWith("X-XSS-Protection", "1; mode=block");
+    expect(res.set).toHaveBeenCalledWith("X-Frame-Options", "DENY");
+    expect(res.set).toHaveBeenCalledWith("X-Content-Type-Options", "nosniff");
+    expect(res.set).toHaveBeenCalledWith(
+      "Cache-control",
+      "public, max-age=600"
     );
 
     expect(next).toHaveBeenCalled();

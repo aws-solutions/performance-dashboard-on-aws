91f45ce5cb58284cbe708b3b5f9e05da3b005aab
trietlu <trietlu@amazon.com>
2021-02-08 23:55:20 -0500

    Revert previous addiition of listening to Amplify Hub events

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -15,7 +15,6 @@ interface Props extends cdk.StackProps {
   identityPoolId: string;
   appClientId: string;
   backendApiUrl: string;
-  frontendDomain: string;
 }
 
 export class FrontendStack extends cdk.Stack {
@@ -39,7 +38,7 @@ export class FrontendStack extends cdk.Stack {
     const httpHeaders = new HttpHeaders(this, "HttpHeaders", {
       httpHeaders: {
         "Content-Security-Policy":
-          "default-src 'self'; img-src 'self' blob:; style-src 'unsafe-inline' 'self'; connect-src 'self' https://*.amazonaws.com; block-all-mixed-content;",
+          "default-src 'self'; img-src 'self' blob:; style-src 'unsafe-inline' 'self'; connect-src 'self' https://*.amazoncognito.com https://*.amazonaws.com; block-all-mixed-content;",
         "Strict-Transport-Security": "max-age=31540000; includeSubdomains",
         "X-XSS-Protection": "1; mode=block",
         "X-Frame-Options": "DENY",
@@ -82,7 +81,6 @@ export class FrontendStack extends cdk.Stack {
         ],
       }
     );
-    props.frontendDomain = distribution.distributionDomainName;
 
     /**
      * S3 Deploy
@@ -140,7 +138,7 @@ export class FrontendStack extends cdk.Stack {
         BRAND_NAME: "Performance Dashboard",
         TOPIC_AREA_LABEL: "Topic area",
         TOPIC_AREAS_LABEL: "Topic areas",
-        FRONTEND_DOMAIN: props.frontendDomain,
+        FRONTEND_DOMAIN: "",
         COGNITO_DOMAIN: "",
         SAML_PROVIDER: "",
         ENTERPRISE_LOGIN_LABEL: "Enterprise Sign-In",

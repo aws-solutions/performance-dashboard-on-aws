75d4a97d1431b64006816f2bc4b02e6b114416b7
Fernando Dingler <ferdingler@users.noreply.github.com>
2021-02-17 10:01:41 -0800

    GTT-901 - Implemented i18n foundation (#297)
    * i18n spike
    
    * Defaulting to browser language
    
    * Added example of a number locale
    
    * i18n in setupTests
    
    Co-authored-by: Andrew Mokotoff <amokotoff@gmail.com>

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import english from "./locales/en/translation.json";
+import spanish from "./locales/es/translation.json";
+
+export default function init() {
+  i18n
+    .use(initReactI18next)
+    // For all configuration options read:
+    // https://www.i18next.com/overview/configuration-options
+    .init({
+      lng: window.navigator.language || "en",
+      fallbackLng: "en",
+      debug: false,
+      resources: {
+        en: { translation: english },
+        es: { translation: spanish },
+      },
+    });
+}

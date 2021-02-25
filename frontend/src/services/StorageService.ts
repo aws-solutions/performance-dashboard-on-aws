d7564b49c6f609729849938ec3cd8c0fb00d5375
Andrew Mokotoff <amokotoff@gmail.com>
2021-02-25 15:09:16 -0500

    Fixed the storage servi e refactor that prevented uploads to datasets bucket, and added an image preview to the edit logo screen

--- frontend/src/services/StorageService.ts
+++ frontend/src/services/StorageService.ts
@@ -37,14 +37,18 @@ async function downloadFile(
   path: string,
   alternativeBucket?: string
 ): Promise<File> {
-  // Note: if alternativeBucket is undefined, it will get routed to datasets by default.
-  const data: any = await Storage.get(path, {
-    bucket: alternativeBucket,
-    download: true,
-    level: accessLevel,
-    serverSideEncryption,
-  });
-
+  const data: any = alternativeBucket
+    ? await Storage.get(path, {
+        bucket: alternativeBucket,
+        download: true,
+        level: accessLevel,
+        serverSideEncryption,
+      })
+    : await Storage.get(path, {
+        download: true,
+        level: accessLevel,
+        serverSideEncryption,
+      });
   if (!data || !data.Body) {
     throw new Error(
       "The query using the provided S3 key, returned no results."

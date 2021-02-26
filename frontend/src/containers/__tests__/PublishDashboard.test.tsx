f93a4303c5fd5516f76810ce1000ee473b9d3f60
Fernando Dingler <fdingler@amazon.com>
2021-02-26 14:48:21 -0800

    Save release notes when user moves to step 2

--- frontend/src/containers/__tests__/PublishDashboard.test.tsx
+++ frontend/src/containers/__tests__/PublishDashboard.test.tsx
@@ -1,39 +1,46 @@
 import React from "react";
-import { render, RenderResult, fireEvent, act } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { render, fireEvent, act, screen } from "@testing-library/react";
+import { Router, Route } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import BackendService from "../../services/BackendService";
 import PublishDashboard from "../PublishDashboard";
 
 jest.mock("../../services/BackendService");
 jest.mock("../../hooks");
 
-let wrapper: RenderResult;
 beforeEach(() => {
-  wrapper = render(<PublishDashboard />, {
-    wrapper: MemoryRouter,
-  });
+  const history = createMemoryHistory();
+  history.push("/admin/dashboard/123/publish");
+
+  render(
+    <Router history={history}>
+      <Route path="/admin/dashboard/:dashboardId/publish">
+        <PublishDashboard />
+      </Route>
+    </Router>
+  );
 });
 
 test("renders dashboard title", () => {
   expect(
-    wrapper.getByRole("heading", {
+    screen.getByRole("heading", {
       name: "My AWS Dashboard",
     })
   ).toBeInTheDocument();
 });
 
 test("renders topic area", () => {
-  expect(wrapper.getByText("Bananas")).toBeInTheDocument();
+  expect(screen.getByText("Bananas")).toBeInTheDocument();
 });
 
 test("renders step indicator in step 1", () => {
   expect(
-    wrapper.getByRole("heading", { name: "Step 1 of 2 Internal version notes" })
+    screen.getByRole("heading", { name: "Step 1 of 2 Internal version notes" })
   ).toBeInTheDocument();
 });
 
-test("continue button advances to step 2", async () => {
-  fireEvent.input(wrapper.getByLabelText("Internal release notes"), {
+test("continue button advances to step 2 and saves releaseNotes", async () => {
+  fireEvent.input(screen.getByLabelText("Internal release notes"), {
     target: {
       value: "Some release notes",
     },
@@ -41,39 +48,45 @@ test("continue button advances to step 2", async () => {
 
   await act(async () => {
     fireEvent.click(
-      wrapper.getByRole("button", {
+      screen.getByRole("button", {
         name: "Continue",
       })
     );
   });
 
+  expect(BackendService.publishPending).toBeCalledWith(
+    "123",
+    expect.anything(),
+    "Some release notes"
+  );
+
   expect(
-    wrapper.getByRole("heading", {
+    screen.getByRole("heading", {
       name: "Step 2 of 2 Review and publish",
     })
   ).toBeInTheDocument();
 });
 
 test("publish button invokes BackendService", async () => {
-  fireEvent.input(wrapper.getByLabelText("Internal release notes"), {
+  fireEvent.input(screen.getByLabelText("Internal release notes"), {
     target: {
       value: "Some release notes",
     },
   });
 
   fireEvent.click(
-    wrapper.getByRole("button", {
+    screen.getByRole("button", {
       name: "Continue",
     })
   );
 
   await act(async () => {
-    fireEvent.click(wrapper.getByTestId("AcknowledgementCheckboxLabel"));
+    fireEvent.click(screen.getByTestId("AcknowledgementCheckboxLabel"));
   });
 
   await act(async () => {
     fireEvent.click(
-      wrapper.getByRole("button", {
+      screen.getByRole("button", {
         name: "Publish",
       })
     );
@@ -85,7 +98,7 @@ test("publish button invokes BackendService", async () => {
 test("return to draft button invokes BackendService", async () => {
   await act(async () => {
     fireEvent.click(
-      wrapper.getByRole("button", {
+      screen.getByRole("button", {
         name: "Return to draft",
       })
     );

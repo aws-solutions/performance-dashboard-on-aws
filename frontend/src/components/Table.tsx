e6a963b164eab747a205a4c28154b65cbfc3450e
Miguel Pavon Diaz <migdizn@amazon.com>
2021-03-03 13:15:27 -0500

    Add pagination to table component

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -6,6 +6,7 @@ import {
   useSortBy,
   useRowSelect,
   useGlobalFilter,
+  usePagination,
 } from "react-table";
 
 interface Props {
@@ -46,8 +47,17 @@ function Table(props: Props) {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
     selectedFlatRows,
     setGlobalFilter,
   } = useTable(
@@ -57,11 +67,14 @@ function Table(props: Props) {
       disableSortRemove: true,
       initialState: {
         selectedRowIds: {},
+        pageIndex: 0,
+        pageSize: 25,
         sortBy: initialSortBy,
       },
     },
     useGlobalFilter,
     useSortBy,
+    usePagination,
     useRowSelect,
     (hooks) => {
       if (props.selection !== "none") {
@@ -101,81 +114,136 @@ function Table(props: Props) {
   }, [selectedFlatRows, onSelection]);
 
   return (
-    <table
-      className={`usa-table usa-table--borderless${className}`}
-      width={props.width}
-      {...getTableProps()}
-    >
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column, i) => (
-              <th
-                scope="col"
-                {...column.getHeaderProps()}
-                style={
-                  props.selection !== "none"
-                    ? { padding: "0.5rem 1rem" }
-                    : { minWidth: column.minWidth }
-                }
-              >
-                <span>
-                  {
-                    /**
-                     * The split is to remove the quotes from the
-                     * string, the filter to remove the resulted
-                     * empty ones, and the join to form it again.
-                     */
-                    typeof column.render("Header") === "string"
-                      ? (column.render("Header") as string)
-                          .split('"')
-                          .filter(Boolean)
-                          .join()
-                      : column.render("Header")
+    <>
+      <table
+        className={`usa-table usa-table--borderless${className}`}
+        width={props.width}
+        {...getTableProps()}
+      >
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, i) => (
+                <th
+                  scope="col"
+                  {...column.getHeaderProps()}
+                  style={
+                    props.selection !== "none"
+                      ? { padding: "0.5rem 1rem" }
+                      : { minWidth: column.minWidth }
                   }
-                </span>
-                {props.selection !== "none" && i === 0 ? null : (
-                  <button
-                    className="margin-left-1 usa-button usa-button--unstyled"
-                    {...column.getSortByToggleProps()}
-                    title={`Toggle SortBy ${column.Header}`}
-                  >
-                    <FontAwesomeIcon
-                      className={`hover:text-base-light ${
-                        column.isSorted ? "text-base-darkest" : "text-white"
-                      }`}
-                      icon={
-                        column.isSorted && column.isSortedDesc
-                          ? faChevronDown
-                          : faChevronUp
-                      }
-                    />
-                  </button>
-                )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell, j) => {
-                return j === 0 && props.selection === "none" ? (
-                  <th scope="row" {...cell.getCellProps()}>
-                    {cell.render("Cell")}
-                  </th>
-                ) : (
-                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                );
-              })}
+                >
+                  <span>
+                    {
+                      /**
+                       * The split is to remove the quotes from the
+                       * string, the filter to remove the resulted
+                       * empty ones, and the join to form it again.
+                       */
+                      typeof column.render("Header") === "string"
+                        ? (column.render("Header") as string)
+                            .split('"')
+                            .filter(Boolean)
+                            .join()
+                        : column.render("Header")
+                    }
+                  </span>
+                  {props.selection !== "none" && i === 0 ? null : (
+                    <button
+                      className="margin-left-1 usa-button usa-button--unstyled"
+                      {...column.getSortByToggleProps()}
+                      title={`Toggle SortBy ${column.Header}`}
+                    >
+                      <FontAwesomeIcon
+                        className={`hover:text-base-light ${
+                          column.isSorted ? "text-base-darkest" : "text-white"
+                        }`}
+                        icon={
+                          column.isSorted && column.isSortedDesc
+                            ? faChevronDown
+                            : faChevronUp
+                        }
+                      />
+                    </button>
+                  )}
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell, j) => {
+                  return j === 0 && props.selection === "none" ? (
+                    <th scope="row" {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </th>
+                  ) : (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="padding-1 grid-row">
+        <div className="grid-col-4 text-left">
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {"<<"}
+          </button>{" "}
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {"<"}
+          </button>{" "}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {">"}
+          </button>{" "}
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {">>"}
+          </button>{" "}
+        </div>
+        <div className="grid-col-4 text-center">
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <span>
+            | Go to page:{" "}
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: "100px" }}
+            />
+          </span>
+        </div>
+        <div className="grid-col-4 text-right">
+          <select
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[1, 5, 10, 20, 25].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </>
   );
 }
 

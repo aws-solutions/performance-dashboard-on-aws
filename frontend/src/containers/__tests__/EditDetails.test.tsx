32157fec0fd8a962f692af7558d01e60c499b0ff
Fernando Dingler <ferdingler@users.noreply.github.com>
2021-03-11 06:43:59 -0800

    GTT-721 Re-styling create and edit dashboard pages (#349)
    * GTT-721 Re-styling create dashboard page
    
    * Re-styled edit details screen
    
    * Added test for preview

--- frontend/src/containers/__tests__/EditDetails.test.tsx
+++ frontend/src/containers/__tests__/EditDetails.test.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { render, fireEvent, act, screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  act,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import { createMemoryHistory } from "history";
-import { MemoryRouter, Route, Router } from "react-router-dom";
+import { Route, Router } from "react-router-dom";
 import BackendService from "../../services/BackendService";
 import EditDetails from "../EditDetails";
 
@@ -45,7 +51,22 @@ test("invokes cancel function when use clicks cancel", async () => {
   expect(history.push).toHaveBeenCalledWith("/admin/dashboard/edit/123");
 });
 
-test("edit details should match snapshot", async () => {
-  const wrapper = render(<EditDetails />, { wrapper: MemoryRouter });
-  expect(wrapper.container).toMatchSnapshot();
+test("renders a preview of dashboard name and description", async () => {
+  fireEvent.input(screen.getByLabelText("Dashboard Name"), {
+    target: {
+      value: "Foo Bar",
+    },
+  });
+
+  fireEvent.input(screen.getByLabelText("Description - optional"), {
+    target: {
+      value: "FizzBuzz",
+    },
+  });
+
+  const description = screen.getByText("FizzBuzz");
+  const name = screen.getByRole("heading", { name: "Foo Bar" });
+
+  await waitFor(() => expect(name).toBeInTheDocument());
+  await waitFor(() => expect(description).toBeInTheDocument());
 });

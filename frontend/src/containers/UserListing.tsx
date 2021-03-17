5099a190d219d7930bcca6cc3279f927a28822c5
Fernando Dingler <fdingler@amazon.com>
2021-03-17 14:26:30 -0700

    fix: User list wasn't refreshed after deleting

--- frontend/src/containers/UserListing.tsx
+++ frontend/src/containers/UserListing.tsx
@@ -16,7 +16,7 @@ const MenuItem = DropdownMenu.MenuItem;
 
 function UserListing() {
   const history = useHistory<LocationState>();
-  const { users, reloadUsers } = useUsers();
+  const { users, reloadUsers, setUsers } = useUsers();
   const [filter, setFilter] = useState("");
   const [selected, setSelected] = useState<Array<User>>([]);
   const [isOpenResendInviteModal, setIsOpenResendInviteModal] = useState(false);
@@ -34,8 +34,8 @@ function UserListing() {
 
   const removeUsers = async () => {
     if (selected.length) {
+      const usernames = selected.map((user) => user.userId);
       try {
-        const usernames = selected.map((user) => user.userId);
         await BackendService.removeUsers(usernames);
         history.replace("/admin/users", {
           alert: {
@@ -51,7 +51,12 @@ function UserListing() {
           },
         });
       } finally {
+        // Reload users but also update the UI optimistically because
+        // the backend sometimes returns the same list of users including
+        // the deleted one due to eventual consistency.
         await reloadUsers();
+        setUsers(users.filter((user) => !usernames.includes(user.userId)));
+
         setIsOpenRemoveUsersModal(false);
       }
     }

8e13899c539c08320754fdcdeb41485014fe1282
Fernando Dingler <fdingler@amazon.com>
2021-04-05 15:00:41 -0700

    Preview content item when editor makes selection

--- frontend/src/containers/__tests__/AddContent.test.tsx
+++ frontend/src/containers/__tests__/AddContent.test.tsx
@@ -1,67 +1,74 @@
 import React from "react";
-import { render, fireEvent, act } from "@testing-library/react";
+import { render, fireEvent, act, screen } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { MemoryRouter, Router } from "react-router-dom";
 import AddContent from "../AddContent";
 
-test("renders Add content", async () => {
-  const { getByRole } = render(<AddContent />, { wrapper: MemoryRouter });
-  const addContent = await getByRole("heading", { name: "Add content item" });
-  expect(addContent).toBeInTheDocument();
-});
+describe("Add content screen", () => {
+  beforeEach(() => {
+    render(<AddContent />, { wrapper: MemoryRouter });
+  });
 
-test("renders Step 1 of 2", async () => {
-  const { findByText } = render(<AddContent />, { wrapper: MemoryRouter });
-  const stepOneOfTwo = await findByText("Step 1 of 2");
-  expect(stepOneOfTwo).toBeInTheDocument();
-});
+  test("renders Add content", async () => {
+    const addContent = await screen.getByRole("heading", {
+      name: "Add content item",
+    });
+    expect(addContent).toBeInTheDocument();
+  });
 
-test("renders text option", async () => {
-  const { findByText } = render(<AddContent />, { wrapper: MemoryRouter });
-  const header = await findByText("Text");
-  const description = await findByText(
-    "Add a formatted block of text. Input supports Markdown including links, bullets, bolding, and headings."
-  );
-  expect(header).toBeInTheDocument();
-  expect(description).toBeInTheDocument();
-});
+  test("renders Step 1 of 2", async () => {
+    const stepOneOfTwo = await screen.findByText("Step 1 of 2");
+    expect(stepOneOfTwo).toBeInTheDocument();
+  });
 
-test("renders metrics option", async () => {
-  const { findByText } = render(<AddContent />, { wrapper: MemoryRouter });
-  const header = await findByText("Metrics");
-  const description = await findByText(
-    "Add one or more metrics to show point-in-time numerical data and trends."
-  );
-  expect(header).toBeInTheDocument();
-  expect(description).toBeInTheDocument();
-});
+  test("renders text option", async () => {
+    const header = await screen.findByText("Text");
+    const description = await screen.findByText(
+      "Add a formatted block of text. Input supports Markdown including links, bullets, bolding, and headings."
+    );
+    expect(header).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
 
-test("renders chart option", async () => {
-  const { findByText } = render(<AddContent />, { wrapper: MemoryRouter });
-  const header = await findByText("Chart");
-  const description = await findByText(
-    "Display data as a visualization, including line, bar, column and part-to-whole charts."
-  );
-  expect(header).toBeInTheDocument();
-  expect(description).toBeInTheDocument();
-});
+  test("renders metrics option", async () => {
+    const header = await screen.findByText("Metrics");
+    const description = await screen.findByText(
+      "Add one or more metrics to show point-in-time numerical data and trends."
+    );
+    expect(header).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
 
-test("renders table option", async () => {
-  const { findByText } = render(<AddContent />, { wrapper: MemoryRouter });
-  const header = await findByText("Table");
-  const description = await findByText(
-    "Display data formatted in rows and columns."
-  );
-  expect(header).toBeInTheDocument();
-  expect(description).toBeInTheDocument();
-});
+  test("renders chart option", async () => {
+    const header = await screen.findByText("Chart");
+    const description = await screen.findByText(
+      "Display data as a visualization, including line, bar, column and part-to-whole charts."
+    );
+    expect(header).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
 
-test("renders image option", async () => {
-  const { findByText } = render(<AddContent />, { wrapper: MemoryRouter });
-  const header = await findByText("Image");
-  const description = await findByText("Upload an image to display.");
-  expect(header).toBeInTheDocument();
-  expect(description).toBeInTheDocument();
+  test("renders table option", async () => {
+    const header = await screen.findByText("Table");
+    const description = await screen.findByText(
+      "Display data formatted in rows and columns."
+    );
+    expect(header).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
+
+  test("renders image option", async () => {
+    const header = await screen.findByText("Image");
+    const description = await screen.findByText("Upload an image to display.");
+    expect(header).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
+
+  test("renders preview thumbnail for each content item", async () => {
+    fireEvent.click(screen.getByLabelText("Text"));
+    const thumbnail = screen.getByAltText("Text Content Item Preview");
+    expect(thumbnail).toBeInTheDocument();
+  });
 });
 
 test("cancel link takes you to Edit Dashboard screen", async () => {

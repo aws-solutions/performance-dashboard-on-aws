e37b9b305cfdb0af716d3dd19f3a743728e40414
Jason Gudalis <73137674+jasongudalis@users.noreply.github.com>
2021-04-11 12:51:53 -0400

    Added localization to FourZeroFour
    Updated page to pull from localized text

--- frontend/src/containers/FourZeroFour.tsx
+++ frontend/src/containers/FourZeroFour.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Link from "../components/Link";
 import Footer from "../layouts/Footer";
+import { useTranslation } from "react-i18next";
 
 function FourZeroFour() {
+  const { t } = useTranslation();
+  
   return (
     <>
       <div className="text-center">
         <p className="font-sans-3xl text-heavy margin-top-9 margin-bottom-1">
-          404 / Page not found
+          {t("PageNotFound")}
         </p>
         <hr className="width-tablet border-base-light" />
         <p className="font-sans-md">
-          You may want to double-check your link and try again, or return to the{" "}
+          {t("PageNotFoundDescription")}{" "}
           <Link to="/">homepage</Link>.
         </p>
       </div>

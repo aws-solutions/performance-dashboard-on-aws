d4f7492896d1a09bc4dfe30f7f8864d50e9b4492
Miguel Pavon Diaz <71112226+miguelpdiaz8@users.noreply.github.com>
2021-05-20 08:54:59 -0400

    Add drag and drop for widgets list (#470)

--- frontend/src/components/__tests__/ContentItem.test.tsx
+++ frontend/src/components/__tests__/ContentItem.test.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ContentItem from "../ContentItem";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 test("renders a content item", async () => {
   const wrapper = render(
-    <ContentItem className="margin-y-1">
-      <p>Test</p>
-    </ContentItem>
+    <DndProvider backend={HTML5Backend}>
+      <ContentItem
+        className="margin-y-1"
+        id="test"
+        index={0}
+        moveWidget={() => {}}
+        onDrop={() => {}}
+      >
+        <p>Test</p>
+      </ContentItem>
+    </DndProvider>
   );
   expect(wrapper.container).toMatchSnapshot();
 });

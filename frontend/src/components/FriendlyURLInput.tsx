78cac5ffa19ca22d5242bb5cf027dc4c666d338c
Aakash Balaji <aakbalaj@amazon.com>
2021-06-01 14:55:19 -0700

    removed changes from input/prefix

--- frontend/src/components/FriendlyURLInput.tsx
+++ frontend/src/components/FriendlyURLInput.tsx
@@ -80,23 +80,16 @@ function FriendlyURLInput({ value, onChange, showWarning }: Props) {
                 {t("EditURLComponent.Error")}
               </span>
             )}
-
-            <div className="input-group prefix">
-              <span className="input-group-addon">
-                {window.location.protocol}//{window.location.hostname}/
-              </span>
-              <input
-                id="friendlyURL"
-                className="has-prefix"
-                name="friendlyURL"
-                type="text"
-                onChange={(e: any) => sanitizeUrl(e.target.value)}
-                style={{ content: "hi" }}
-                ref={register({
-                  required: true,
-                })}
-              />
-            </div>
+            <input
+              id="friendlyURL"
+              className="usa-input"
+              name="friendlyURL"
+              type="text"
+              onChange={(e: any) => sanitizeUrl(e.target.value)}
+              ref={register({
+                required: true,
+              })}
+            />
           </div>
         }
       />

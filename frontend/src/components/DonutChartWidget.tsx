bd720c6a47bbd51c575058d0723d2aed90d0c6bc
yinxudeng <47926598+yinxudeng@users.noreply.github.com>
2021-07-30 08:01:36 -0700

    GTT-1483: Improved mobile display of pie/donut chart (#557)

--- frontend/src/components/DonutChartWidget.tsx
+++ frontend/src/components/DonutChartWidget.tsx
@@ -8,7 +8,7 @@ import {
   Tooltip,
   Label,
 } from "recharts";
-import { useColors } from "../hooks";
+import { useColors, useWindowSize } from "../hooks";
 import TickFormatter from "../services/TickFormatter";
 import MarkdownRender from "./MarkdownRender";
 import DataTable from "./DataTable";
@@ -218,6 +218,33 @@ const DonutChartWidget = (props: Props) => {
     );
   };
 
+  const windowSize = useWindowSize();
+  const smallScreenPixels = 800;
+
+  const calculateChartHeight = (): number => {
+    const baseHeight = 300;
+    const pixelsByPart = 60;
+    const pixelsByPartInPreview = 50;
+    const labelsPerRow = 4;
+    const labelsPerRowInPreview = 2;
+
+    if (!data || !data.length) {
+      return baseHeight;
+    }
+
+    let additional;
+    if (windowSize.width <= smallScreenPixels || showMobilePreview) {
+      additional = data.length * pixelsByPart;
+    } else if (props.isPreview) {
+      additional =
+        (Math.floor(data.length / labelsPerRowInPreview) + 1) *
+        pixelsByPartInPreview;
+    } else {
+      additional = (Math.floor(data.length / labelsPerRow) + 1) * pixelsByPart;
+    }
+    return baseHeight + additional;
+  };
+
   return (
     <div>
       <h2 className={`margin-bottom-${props.summaryBelow ? "4" : "1"}`}>
@@ -230,7 +257,7 @@ const DonutChartWidget = (props: Props) => {
         />
       )}
       {donutData.current.length && (
-        <ResponsiveContainer width="100%" height={420}>
+        <ResponsiveContainer width="100%" height={calculateChartHeight()}>
           <PieChart>
             <Legend
               verticalAlign="top"
@@ -244,6 +271,11 @@ const DonutChartWidget = (props: Props) => {
               onClick={toggleParts}
               onMouseLeave={() => setPartsHover(null)}
               onMouseEnter={(e: any) => setPartsHover(e.value)}
+              layout={
+                windowSize.width <= smallScreenPixels || showMobilePreview
+                  ? "vertical"
+                  : undefined
+              }
             />
             <Pie
               data={donutData.current.map((d: any) => {
@@ -253,7 +285,13 @@ const DonutChartWidget = (props: Props) => {
               })}
               dataKey="value"
               nameKey="name"
-              cx={props.isPreview ? "50%" : "28%"}
+              cx={
+                props.isPreview ||
+                windowSize.width <= smallScreenPixels ||
+                showMobilePreview
+                  ? "50%"
+                  : "28%"
+              }
               cy="50%"
               outerRadius={120}
               innerRadius={80}

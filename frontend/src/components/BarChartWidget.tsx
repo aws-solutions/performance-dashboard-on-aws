9fd3c1161a780ae7544f472c008748824b476bda
yinxudeng <47926598+yinxudeng@users.noreply.github.com>
2021-08-11 10:22:05 -0700

    GTT-1516: [Bug] Fixed missing labels in bar chart when there is too much data (#562)
    * bar chart height scales dynamically with the number of data
    
    * trigger checks
    
    * externalized logic to calculate chart height and added comments
    
    * refactored code
    
    * updated code logic
    
    Co-authored-by: Triet <triet05@gmail.com>

--- frontend/src/components/BarChartWidget.tsx
+++ frontend/src/components/BarChartWidget.tsx
@@ -96,6 +96,28 @@ const BarChartWidget = (props: Props) => {
   const formatYAxisLabel = (label: string) =>
     label.length > 27 ? label.substr(0, 27).concat("...") : label;
 
+  const calculateChartHeight = (): number => {
+    // When there are 15 rows of data and each row has 3 columns (excluding row
+    // name), having a chart height of 400px is still visually appealing to users.
+    // Adding more rows or columns would require additional height increments.
+    const defaultNumRows = 15;
+    const defaultNumCols = 3;
+    const unitHeight = 400;
+    let multiplicity;
+
+    if (data && data.length) {
+      const numRows = data.length;
+      const numCols = Object.keys(data[0]).length - 1;
+      const rowMultiplicity = Math.floor((numRows - 1) / defaultNumRows) + 1;
+      const colMultiplicity = Math.floor((numCols - 1) / defaultNumCols) + 1;
+      multiplicity = rowMultiplicity * colMultiplicity;
+    } else {
+      multiplicity = 1;
+    }
+
+    return unitHeight * multiplicity;
+  };
+
   return (
     <div>
       <h2 className={`margin-bottom-${props.summaryBelow ? "4" : "1"}`}>
@@ -108,15 +130,7 @@ const BarChartWidget = (props: Props) => {
         />
       )}
       {data && data.length && (
-        <ResponsiveContainer
-          width="100%"
-          height={
-            (data && data.length > 15) ||
-            (data && data[0] && Object.keys(data[0]).length > 4)
-              ? 800
-              : 400
-          }
-        >
+        <ResponsiveContainer width="100%" height={calculateChartHeight()}>
           <BarChart
             className="bar-chart"
             data={props.data}

3a2a0afefc34f0dcf0b1ead748e0b1d4a0c5960a
yinxudeng <47926598+yinxudeng@users.noreply.github.com>
2021-08-20 16:06:34 -0700

    GTT-1502: Improve visual design of metrics content item (#577)
    * symbol is now half the size of number; number now uses the primary color
    
    * removed color for symbol
    
    * added checkbox to center numbers on Add metrics page and Edit metrics page
    
    * remove useScrollUp hook from EditMetrics
    
    * use textAlign to center texts
    
    * refactored code
    
    * updated styling
    
    * cleaned up code

--- frontend/src/components/MetricsCardGroup.tsx
+++ frontend/src/components/MetricsCardGroup.tsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { Metric } from "../models";
+import { Metric, NumberDataType } from "../models";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import TickFormatter from "../services/TickFormatter";
 import dayjs from "dayjs";
 import { useTranslation } from "react-i18next";
+import { useColors } from "../hooks";
 
 interface Props {
   metrics: Array<Metric>;
   metricPerRow: number;
   significantDigitLabels: boolean;
+  metricsCenterAlign: boolean;
 }
 
 function MetricsCardGroup(props: Props) {
   const { t } = useTranslation();
+  const primaryColor = useColors(1)[0];
+
   let rows = [];
   for (let i = 0; i < props.metrics.length; i += props.metricPerRow) {
     let row = [];
@@ -33,7 +37,10 @@ function MetricsCardGroup(props: Props) {
   }
 
   return (
-    <div className="grid-col">
+    <div
+      className="grid-col"
+      style={props.metricsCenterAlign ? { textAlign: "center" } : {}}
+    >
       {rows.map((row, i) => {
         return (
           <div key={i} className="grid-row flex-column">
@@ -55,7 +62,10 @@ function MetricsCardGroup(props: Props) {
                         data-position="bottom"
                         title={metric.value ? metric.value.toString() : ""}
                       >
-                        <h1 className="margin-0 text-no-wrap">
+                        <h1
+                          className="margin-0 text-no-wrap"
+                          style={{ color: primaryColor }}
+                        >
                           {TickFormatter.formatNumber(
                             Number(metric.value),
                             Number(metric.value),

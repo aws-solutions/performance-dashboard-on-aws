5e6c53d3823d73bbdf0a07b572f6b404470073de
Miguel Pavon Diaz <miguelpdiaz8@gmail.com>
2021-09-03 17:48:58 -0400

    Add scroll and arrows to horizontal tabs

--- frontend/src/components/__tests__/Tabs.test.tsx
+++ frontend/src/components/__tests__/Tabs.test.tsx
@@ -2,55 +2,84 @@ import React from "react";
 import { render, within } from "@testing-library/react";
 import Tabs from "../Tabs";
 import { MemoryRouter } from "react-router-dom";
+import { MockedObserver, traceMethodCalls } from "../../testUtils";
+import type { IntersectionObserverCB } from "../../testUtils";
 
-test("renders the Tabs component", async () => {
-  const wrapper = render(
-    <Tabs defaultActive="tab1">
-      <div id="tab1" label="Tab 1">
-        Tab 1
-      </div>
-      <div id="tab2" label="Tab 2">
-        Tab 2
-      </div>
-    </Tabs>
-  );
-  expect(wrapper.container).toMatchSnapshot();
-});
+describe("Tabs tests", () => {
+  let observer: any;
+  let mockedObserverCalls: { [k: string]: any } = {};
+  beforeEach(() => {
+    Object.defineProperty(window, "IntersectionObserver", {
+      writable: true,
+      value: jest
+        .fn()
+        .mockImplementation(function TrackMock(
+          cb: IntersectionObserverCB,
+          options: IntersectionObserverInit
+        ) {
+          observer = traceMethodCalls(
+            new MockedObserver(cb, options),
+            mockedObserverCalls
+          );
 
-test("renders the Tabs component with default tab 2 selected", async () => {
-  const wrapper = render(
-    <Tabs defaultActive="tab2">
-      <div id="tab1" label="Tab 1">
-        Tab 1
-      </div>
-      <div id="tab2" label="Tab 2">
-        Tab 2
-      </div>
-    </Tabs>
-  );
-  expect(wrapper.container).toMatchSnapshot();
-});
+          return observer;
+        }),
+    });
+  });
+  afterEach(() => {
+    observer = null;
+    mockedObserverCalls = {};
+  });
 
-test("renders the tabs", async () => {
-  const { getAllByRole } = render(
-    <Tabs defaultActive="tab2">
-      <div id="tab1" label="Tab 1">
-        Tab 1
-      </div>
-      <div id="tab2" label="Tab 2">
-        Tab 2
-      </div>
-    </Tabs>,
-    {
-      wrapper: MemoryRouter,
-    }
-  );
+  test("renders the Tabs component", async () => {
+    const wrapper = render(
+      <Tabs defaultActive="tab1">
+        <div id="tab1" label="Tab 1">
+          Tab 1
+        </div>
+        <div id="tab2" label="Tab 2">
+          Tab 2
+        </div>
+      </Tabs>
+    );
+    expect(wrapper.container).toMatchSnapshot();
+  });
 
-  const listItems = getAllByRole("listitem");
-  expect(listItems).toHaveLength(2);
+  test("renders the Tabs component with default tab 2 selected", async () => {
+    const wrapper = render(
+      <Tabs defaultActive="tab2">
+        <div id="tab1" label="Tab 1">
+          Tab 1
+        </div>
+        <div id="tab2" label="Tab 2">
+          Tab 2
+        </div>
+      </Tabs>
+    );
+    expect(wrapper.container).toMatchSnapshot();
+  });
 
-  listItems.forEach((item, index) => {
-    const { getByText } = within(item);
-    expect(getByText(`Tab ${index + 1}`)).toBeInTheDocument();
+  test("renders the tabs", async () => {
+    const { getAllByRole } = render(
+      <Tabs defaultActive="tab2">
+        <div id="tab1" label="Tab 1">
+          Tab 1
+        </div>
+        <div id="tab2" label="Tab 2">
+          Tab 2
+        </div>
+      </Tabs>,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+
+    const listItems = getAllByRole("listitem");
+    expect(listItems).toHaveLength(2);
+
+    listItems.forEach((item, index) => {
+      const { getByText } = within(item);
+      expect(getByText(`Tab ${index + 1}`)).toBeInTheDocument();
+    });
   });
 });

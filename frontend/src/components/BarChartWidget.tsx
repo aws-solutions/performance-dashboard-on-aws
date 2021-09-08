eaba1b1f2ca278ed5d0ccc0c540dd62d583280d1
Miguel Pavon Diaz <71112226+miguelpdiaz8@users.noreply.github.com>
2021-09-08 15:20:31 -0400

    Option to display bar and column charts as stacked (#605)
    Co-authored-by: Triet <triet05@gmail.com>

--- frontend/src/components/BarChartWidget.tsx
+++ frontend/src/components/BarChartWidget.tsx
@@ -34,6 +34,7 @@ type Props = {
   columnsMetadata: Array<any>;
   hideDataLabels?: boolean;
   showMobilePreview?: boolean;
+  stackedChart?: boolean;
 };
 
 const BarChartWidget = (props: Props) => {
@@ -210,6 +211,7 @@ const BarChartWidget = (props: Props) => {
                     key={index}
                     fillOpacity={getOpacity(bar)}
                     hide={hiddenBars.includes(bar)}
+                    stackId={props.stackedChart ? "a" : `${index}`}
                     isAnimationActive={false}
                   >
                     {!props.hideDataLabels ? (

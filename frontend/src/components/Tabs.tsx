6a875bd7c34ff6139bf4778b6397e60ab80f527b
Miguel Abreu <miabreu@amazon.com>
2021-10-21 17:53:37 -0400

    Remove unused import

--- frontend/src/components/Tabs.tsx
+++ frontend/src/components/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import Tab from "./Tab";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { LeftArrow, RightArrow } from "./Arrows";

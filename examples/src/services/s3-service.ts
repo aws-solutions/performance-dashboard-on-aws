c2a0cf51a2107ce766d831574d076e9790dc0f36
Phil Dettorre <89091415+PhilTower@users.noreply.github.com>
2021-11-04 15:04:13 -0600

    Gtt 1522 (#611)
    * adding exported json
    
    * updated git ignore
    
    * temp save
    
    * updated json files
    
    * fixed naming, running in lambda
    
    * cleanup
    
    * adding cdk elements for deploy
    
    * reorging files
    
    * first try ad deploy script
    
    * fixed deployment of resource and calling lambda after deploy
    
    * dont need method
    
    * pulled out methods to simply insert script
    
    * added unit tests to lambda
    
    * flushed out readme
    
    * flushed out readme
    
    * updated deploy script
    
    * readme update
    
    * fixed variable and template
    
    * prettier write
    
    * fixed install scripte
    
    * not used
    
    * Fix dashboard id assignment
    
    * Please enter the commit message for your changes. Lines starting
    
    * Fix dashboard id assignment
    
    * Fix dashboard id assignment
    
    * Fix dashboard id assignment
    
    * Fix dashboard id assignment
    
    * Major refactor and typescript conversion
    
    * Create build directory on deploy prereq step
    
    * Reset last updated date
    
    * Add spanish dashboard
    
    * Add piechart spanish
    
    * Add line chart spanish
    
    * Refactor examples
    
    * Adding logging information
    
    * More logging and fixing bugs
    
    * Add export capability
    
    * Major refactor to operate overe one file only
    
    * Update english example dashboard
    
    * Add spanish example
    
    * Add portuguese example
    
    * refactor
    
    * Redo folder structure
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Add missing s3 permission
    
    * Refactor prompts
    
    * Tight regex patterns
    
    * Revert unintended changes
    
    * minor fixes
    
    * More fixes
    
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

---
+++ examples/src/services/s3-service.ts
@@ -0,0 +1,90 @@
+import { env } from "../env";
+import { S3 } from "aws-sdk";
+import { DashboardSnapshot } from "../common";
+import {
+  readSnapshot as readSnapshotFromResources,
+  writeResource,
+} from "./fs-service";
+
+const fs = require("fs-extra");
+
+export const downloadResource = async function (name: string, file: string) {
+  const s3 = new S3();
+  let readStream = s3
+    .getObject({
+      Bucket: env.DATASETS_BUCKET,
+      Key: `public/${file}`,
+    })
+    .createReadStream();
+
+  return writeResource(name, file, readStream);
+};
+
+export const copyResource = function (
+  name: string,
+  source: string,
+  destination: string
+): Promise<S3.CopyObjectOutput> {
+  const s3Client = new S3();
+  const sourceFile = `${name}/files/${source}`;
+  const destinationFile = `public/${destination}`;
+  console.log(
+    "s3.copyContent call: {}/{} -> {}/{}",
+    env.EXAMPLES_BUCKET,
+    sourceFile,
+    env.DATASETS_BUCKET,
+    destinationFile
+  );
+  return new Promise((resolve, reject) => {
+    s3Client.copyObject(
+      {
+        Bucket: env.DATASETS_BUCKET,
+        Key: destinationFile,
+        CopySource: `${env.EXAMPLES_BUCKET}/${sourceFile}`,
+      },
+      function (err, data) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log("resource copied: {}", data);
+          resolve(data);
+        }
+      }
+    );
+  });
+};
+
+export const readSnapshot = async function (
+  name: string
+): Promise<DashboardSnapshot> {
+  const s3 = new S3();
+  return new Promise((resolve, reject) => {
+    s3.getObject(
+      {
+        Bucket: env.EXAMPLES_BUCKET,
+        Key: `${name}/snapshot.json`,
+      },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          console.log(`reading from local resources`);
+
+          try {
+            const snapshot = readSnapshotFromResources(name);
+            resolve(snapshot);
+          } catch (e) {
+            reject(err);
+          }
+        }
+
+        const text = data.Body?.toString();
+        if (!text) {
+          reject("No data");
+        }
+
+        resolve(JSON.parse(text as string));
+      }
+    );
+  });
+};

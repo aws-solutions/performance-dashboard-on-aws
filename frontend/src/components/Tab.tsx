30e95e7a534c2b3a8f7385e82029f2fe7cf00b4a
Chow <chowchin@88665a1e3b3b.ant.amazon.com>
2021-11-06 00:05:18 -0500

    cleanup code

--- frontend/src/components/Tab.tsx
+++ frontend/src/components/Tab.tsx
@@ -27,12 +27,13 @@ function Tab(props: Props) {
   let className =
     "display-inline-block padding-x-2 padding-y-105 text-bold font-sans-md";
 
-  let ariaLabelStr =
-    props.container + " " + t("Tab") + " " + props.label.split("(")[0];
+  let ariaLabelStr = `${props.container} ${t("Tab")} ${
+    props.label.split("(")[0]
+  }`;
 
   if (props.activeTab === props.id) {
     className += " border-base-dark border-x-0 border-top-0 border-bottom-05";
-    ariaLabelStr = t("Active") + " " + ariaLabelStr;
+    ariaLabelStr = `${t("Active")} ${ariaLabelStr}`;
   }
 
   return (

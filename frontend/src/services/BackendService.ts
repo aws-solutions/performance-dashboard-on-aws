8957eb1a08f554acf1d75b37a5062510d966004e
Mazen Kharbutli <mazenkh@amazon.com>
2021-11-19 12:24:56 -0600

    Fix for the case where there is no search query

--- frontend/src/services/BackendService.ts
+++ frontend/src/services/BackendService.ts
@@ -68,7 +68,11 @@ async function fetchPublicDashboardByURL(
 async function fetchPublicHomepageWithQuery(
   query: string
 ): Promise<PublicHomepage> {
-  return await API.get(apiName, `public/search?q=${query}`, {});
+  if (query == undefined || query == "") {
+    return API.get(apiName, "public/homepage", {});
+  } else {
+    return await API.get(apiName, `public/search?q=${query}`, {});
+  }
 }
 
 async function fetchTopicAreas() {

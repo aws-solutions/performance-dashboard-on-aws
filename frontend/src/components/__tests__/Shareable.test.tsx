852bfe5d2be7ef30a7766546efa9d25713ca7658
Miguel Alfonso Abreu Ortega <mabreuortega@gmail.com>
2022-02-02 14:20:47 -0500

    Miguel.abreu/gtt 1826 anchor link (#768)
    * Add scroll delay
    
    * Update Shareable.tsx
    
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

--- frontend/src/components/__tests__/Shareable.test.tsx
+++ frontend/src/components/__tests__/Shareable.test.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { fireEvent, render } from "@testing-library/react";
 import Shareable from "../Shareable";
+import { MemoryRouter } from "react-router-dom";
 
 test("renders the content", async () => {
   const { getByText } = render(
-    <Shareable id="1" title="test">
-      Awesome
-    </Shareable>
+    <MemoryRouter>
+      <Shareable id="1" title="test">
+        Awesome
+      </Shareable>
+    </MemoryRouter>
   );
   expect(getByText("Awesome")).toBeInTheDocument();
 });
 
 test("renders the anchor button", async () => {
   const { getByLabelText } = render(
-    <Shareable id="1" title="test">
-      Awesome
-    </Shareable>
+    <MemoryRouter>
+      <Shareable id="1" title="test">
+        Awesome
+      </Shareable>
+    </MemoryRouter>
   );
-
   const copyLink = getByLabelText("Copy link: test");
 
   expect(copyLink).toBeInTheDocument();

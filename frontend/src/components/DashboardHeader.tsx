e249c9c89310e66a5e2b8381443ef22704cf5d2c
Miguel Alfonso Abreu Ortega <mabreuortega@gmail.com>
2022-03-30 18:11:51 -0400

    Replace content header with h2 (#809)
    * Replace content header with h2
    
    * Minor fix
    
    * Update DashboardHeader.test.tsx.snap
    
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

--- frontend/src/components/DashboardHeader.tsx
+++ frontend/src/components/DashboardHeader.tsx
@@ -29,26 +29,30 @@ function DashboardHeader(props: Props) {
         >
           {props.name}
         </h1>
-        {props.isMobile ? (
-          <div className="margin-top-1">{props.link}</div>
-        ) : (
-          props.link
-        )}
         <div className="text-base text-italic margin-bottom-2">
           {props.topicAreaName}
           {props.lastUpdated &&
             ` | ${t("LastUpdatedLabel")} ${dateFormatter(props.lastUpdated)}`}
         </div>
       </div>
-      <div className={props.unpublished ? "" : "margin-y-2"}>
-        {props.description && (
-          <MarkdownRender
-            source={props.description}
-            className={
-              props.unpublished ? "" : "font-sans-lg usa-prose margin-top-0"
-            }
-          />
-        )}
+      <div className="grid-row">
+        <div
+          className={`${
+            props.isMobile ? "tablet:grid-col-10" : "grid-col flex-12"
+          } ${props.unpublished ? "" : "margin-y-2"}`}
+        >
+          {props.description && (
+            <MarkdownRender
+              source={props.description}
+              className={
+                props.unpublished ? "" : "font-sans-lg usa-prose margin-top-0"
+              }
+            />
+          )}
+        </div>
+        <div className="display-flex flex-row flex-align-self-end margin-bottom-2">
+          <div className="">{props.link}</div>
+        </div>
       </div>
     </div>
   );

c6bf6afff3f696849d99bc4ee8f6520ce7067d5d
Miguel Abreu <mabreuortega@gmail.com>
2022-06-17 19:21:06 -0400

    Miguel.abreu/upgrade node 14 (#830)
    * feat: upgrade to node 14
    
    * chore: remove unused functionality
    
    * chore: more upgrades
    
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -141,7 +141,7 @@ export class FrontendStack extends cdk.Stack {
     // the React code is deployed.
 
     const lambdaFunction = new lambda.Function(this, "EnvConfig", {
-      runtime: lambda.Runtime.NODEJS_12_X,
+      runtime: lambda.Runtime.NODEJS_14_X,
       description: "Deploys env.js file on S3 with environment configuration",
       code: lambda.Code.fromAsset("build/lib/envconfig"),
       handler: "index.handler",

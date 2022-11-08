db9f5c469b177b91aaedbb029a38c2fe08a24c44
Miguel Abreu <mabreuortega@gmail.com>
2022-11-08 11:48:56 -0500

    Update readme badges (#941)
    * Update readme badges
    
    * add supporting files
    
    * Update sonar-project.properties
    
    * update lcov.info paths
    
    * Update sonar-project.properties
    
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

--- test.sh
+++ test.sh
@@ -18,23 +18,10 @@ cd ..
 #
 cd frontend
 echo "Running frontend unit tests"
-CI=true npm run test # Avoid interactive mode on tests
+CI=true npm run test:ci # Avoid interactive mode on tests
 if [ $? -eq 1 ]
 then
   echo "Frontend tests failed"
-  exit 1
-fi
-cd ..
-
-#
-# Insert Examples
-#
-cd examples
-echo "Running examples unit tests"
-npm run test:ci  # Avoid interactive mode on tests
-if [ $? -eq 1 ]
-then
-  echo "Examples tests failed"
   exit 1
 fi
 cd ..

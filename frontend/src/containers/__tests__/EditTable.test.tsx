b76eca7f127b9e3a7e5fa007dd023fc6d0afb590
Denis Quesada <denisqu@amazon.com>
2022-11-11 13:24:25 -0800

    Unit Tests: ËditTable and ViewDashboardAdmin

--- frontend/src/containers/__tests__/EditTable.test.tsx
+++ frontend/src/containers/__tests__/EditTable.test.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import {
+  render,
+  screen,
+  fireEvent,
+  act,
+  waitFor,
+} from "@testing-library/react";
+import { MemoryRouter, Router } from "react-router-dom";
 import BackendService from "../../services/BackendService";
 import StorageService from "../../services/StorageService";
 import EditTable from "../EditTable";
+import { createMemoryHistory } from "history";
+import papaparse from "papaparse";
 
 jest.mock("../../services/BackendService");
 jest.mock("../../services/StorageService");
@@ -12,6 +20,7 @@ jest.mock("../../hooks");
 
 beforeEach(() => {
   BackendService.editWidget = jest.fn();
+  BackendService.createDataset = jest.fn().mockReturnValue({ id: "1244" });
   StorageService.uploadDataset = jest.fn().mockReturnValue({
     s3Keys: {
       raw: "abc.csv",
@@ -38,4 +47,80 @@ test("renders a choose data button", async () => {
   expect(
     await screen.findByRole("tab", { name: "Choose data" })
   ).toBeInTheDocument();
+});
+
+test("on submit, it calls createWidget api and uploads dataset", async () => {
+  const parseSpy = jest.spyOn(papaparse, "parse");
+  const { getByRole, getAllByText, getByLabelText, getByTestId } = render(
+    <EditTable />,
+    {
+      wrapper: MemoryRouter,
+    }
+  );
+
+  await act(async () => {
+    fireEvent.click(getByRole("tab", { name: "Choose data" }));
+  });
+
+  const continueButton = getByRole("button", { name: "Continue" });
+  const radioButton = getByTestId("staticDatasetRadioButton");
+
+  await waitFor(() => {
+    continueButton.removeAttribute("disabled");
+    fireEvent.click(radioButton);
+  });
+
+  const file = new File(["dummy content"], "test.csv", {
+    type: "text/csv",
+  });
+  const uploadFile = getByLabelText("Static datasets");
+  Object.defineProperty(uploadFile, "files", { value: [file] });
+  Object.defineProperty(uploadFile, "value", {
+    value: file.name,
+  });
+  fireEvent.change(uploadFile);
+
+  await act(async () => {
+    fireEvent.click(continueButton);
+  });
+
+  fireEvent.input(getByLabelText("Table title*"), {
+    target: {
+      value: "COVID Cases",
+    },
+  });
+
+  const submitButton = getAllByText("Save")[0];
+
+  await waitFor(() => {
+    expect(parseSpy).toHaveBeenCalled();
+    submitButton.removeAttribute("disabled");
+  });
+
+  await waitFor(() => expect(submitButton).toBeEnabled());
+  await act(async () => {
+    fireEvent.click(submitButton);
+  });
+
+  expect(BackendService.editWidget).toHaveBeenCalled();
+  expect(StorageService.uploadDataset).toHaveBeenCalled();
+  expect(BackendService.createDataset).toHaveBeenCalled();
+});
+
+test("cancel link takes you to Edit Dashboard screen", async () => {
+  const history = createMemoryHistory();
+  jest.spyOn(history, "push");
+
+  const { findByRole } = render(
+    <Router history={history}>
+      <EditTable />
+    </Router>
+  );
+
+  await act(async () => {
+    const cancelButton = await findByRole("button", { name: "Cancel" });
+    fireEvent.click(cancelButton);
+  });
+
+  expect(history.push).toHaveBeenCalledWith("/admin/dashboard/edit/undefined");
 });
--- frontend/src/containers/__tests__/ViewDashboardAdmin.test.tsx
+++ frontend/src/containers/__tests__/ViewDashboardAdmin.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { DashboardState } from "../../models";
 import ViewDashboardAdmin from "../ViewDashboardAdmin";
@@ -19,8 +19,103 @@ const dummyDashboard = {
   createdBy: "test user",
   state: DashboardState.Published,
   version: 1,
-  widgets: [],
+  widgets: [
+    {
+      id: "0828b98e",
+      dashboardId: "123",
+      name: "Column chart: Average class size",
+      widgetType: "Chart",
+      order: 5,
+      updatedAt: "2022-09-13T05:27:20.333Z",
+      showTitle: true,
+      section: "",
+      content: {
+        summary: "A column chart ...",
+        fileName: "Average class size.csv",
+        sortByDesc: false,
+        title: "Column chart: Average class size",
+        significantDigitLabels: false,
+        dataLabels: false,
+        s3Key: {
+          raw: "672f377a-1def-4308-ab80-a2b15b0d91a9.csv",
+          json: "9ac17541-9ba5-4fac-af5c-401f33893195.json",
+        },
+        summaryBelow: false,
+        horizontalScroll: false,
+        showTotal: true,
+        chartType: "ColumnChart",
+        datasetId: "a8bec0ec-9fcb-4ef1-ba11-2e027cdb44a8",
+        computePercentages: false,
+        datasetType: "StaticDataset",
+        columnsMetadata: [],
+      },
+    },
+    {
+      id: "ebf83d58-c226-4f82-aad6-7a18d32ac931",
+      dashboardId: "a2eb7bec-a8fc-4055-9d49-669dd178e540",
+      name: "Sections: Grouping related content items together ",
+      widgetType: "Section",
+      order: 6,
+      updatedAt: "2022-09-13T05:27:20.333Z",
+      showTitle: true,
+      section: "",
+      content: {
+        summary: "A section ...",
+        horizontally: false,
+        title: "Sections: Grouping related content items together ",
+        showWithTabs: true,
+        widgetIds: ["3cd7a776-3240-4155-b479-60e49aaae1ce"],
+      },
+    },
+    {
+      id: "3cd7a776-3240-4155-b479-60e49aaae1ce",
+      dashboardId: "a2eb7bec-a8fc-4055-9d49-669dd178e540",
+      name: "Donut chart: Devices used to access service",
+      widgetType: "Chart",
+      order: 7,
+      updatedAt: "2022-09-13T05:27:20.333Z",
+      showTitle: true,
+      section: "ebf83d58-c226-4f82-aad6-7a18d32ac931",
+      content: {
+        summary: "A donut chart...",
+        fileName: "Devices used to access service.csv",
+        sortByDesc: false,
+        title: "Donut chart: Devices used to access service",
+        significantDigitLabels: false,
+        dataLabels: false,
+        s3Key: {
+          raw: "b1dc2e7f-aa52-472b-8fa5-d06a3830d764.csv",
+          json: "29716b9c-6f30-4c80-a4a2-3f12bd5d6e71.json",
+        },
+        summaryBelow: false,
+        showTotal: false,
+        chartType: "DonutChart",
+        datasetId: "fde387fd-ea63-4565-88d3-b28c4474912b",
+        computePercentages: false,
+        datasetType: "StaticDataset",
+        columnsMetadata: [
+          {
+            numberType: "Percentage",
+            hidden: false,
+            columnName: "Usage",
+            dataType: "Number",
+          },
+        ],
+      },
+    },
+  ],
 };
+
+beforeEach(() => {
+  // IntersectionObserver isn't available in test environment
+  const mockIntersectionObserver = jest.fn();
+  mockIntersectionObserver.mockReturnValue({
+    observe: () => null,
+    unobserve: () => null,
+    disconnect: () => null,
+  });
+  window.IntersectionObserver = mockIntersectionObserver;
+});
 
 describe("general dashboard details", () => {
   beforeEach(() => {
@@ -83,6 +178,16 @@ describe("dashboard in draft", () => {
       )
     ).toBeInTheDocument();
   });
+
+  test("renders the publish modal", async () => {
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button", { name: "Publish" }));
+    });
+
+    expect(
+      screen.getByRole("button", { name: "Publish dashboard" })
+    ).toBeInTheDocument();
+  });
 });
 
 describe("dashboard published", () => {
@@ -111,6 +216,11 @@ describe("dashboard published", () => {
 
   test("renders show version notes checkbox", async () => {
     const viewCheckbox = screen.getByText("Show version notes");
+    fireEvent.input(viewCheckbox, {
+      target: {
+        checked: true,
+      },
+    });
     expect(viewCheckbox).toBeInTheDocument();
   });
 });
@@ -123,7 +233,6 @@ describe("dashboard archived", () => {
       loading: false,
       dashboard: archived,
     });
-
     render(<ViewDashboardAdmin />, {
       wrapper: MemoryRouter,
     });
@@ -140,4 +249,211 @@ describe("dashboard archived", () => {
       screen.getByRole("button", { name: "Archived dashboard actions" })
     ).toBeInTheDocument();
   });
+
+  test("renders the re-publish modal", async () => {
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button", { name: "Re-publish" }));
+    });
+
+    const message = await screen.findByText(
+      "Are you sure you want to re-publish this dashboard?"
+    );
+    expect(message).toBeInTheDocument();
+  });
+});
+
+describe("dashboard in draft in mobile", () => {
+  beforeEach(() => {
+    // Override the useDashboard mock to return a dashboard in Draft
+    const draft = { ...dummyDashboard, state: DashboardState.Draft };
+    (hooks.useDashboard as any) = jest.fn().mockReturnValue({
+      loading: false,
+      dashboard: draft,
+    });
+    (hooks.useWindowSize as any) = jest.fn().mockReturnValue({
+      width: 400,
+    });
+
+    render(<ViewDashboardAdmin />, {
+      wrapper: MemoryRouter,
+    });
+  });
+
+  test("renders a close preview button", async () => {
+    expect(
+      screen.getByRole("button", { name: "Close Preview" })
+    ).toBeInTheDocument();
+  });
+
+  test("renders a publish button", async () => {
+    expect(screen.getByRole("button", { name: "Publish" })).toBeInTheDocument();
+  });
+
+  test("renders an information alert", async () => {
+    expect(
+      screen.getByText(
+        "Below is a preview of what the published dashboard will look like. " +
+          "If ready to proceed, you can publish the dashboard to make it " +
+          "available on the published site."
+      )
+    ).toBeInTheDocument();
+  });
+
+  test("renders the publish modal", async () => {
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button", { name: "Publish" }));
+    });
+
+    expect(
+      screen.getByRole("button", { name: "Publish dashboard" })
+    ).toBeInTheDocument();
+  });
+});
+
+describe("dashboard published in mobile", () => {
+  beforeEach(() => {
+    // Override the useDashboard mock to return a dashboard in Published state
+    const published = { ...dummyDashboard, state: DashboardState.Published };
+    (hooks.useDashboard as any) = jest.fn().mockReturnValue({
+      loading: false,
+      dashboard: published,
+    });
+    (hooks.useWindowSize as any) = jest.fn().mockReturnValue({
+      width: 400,
+    });
+
+    render(<ViewDashboardAdmin />, {
+      wrapper: MemoryRouter,
+    });
+  });
+
+  test("renders an archive button", async () => {
+    expect(
+      screen.getByRole("button", { name: "Published dashboard actions" })
+    ).toBeInTheDocument();
+  });
+
+  test("renders an update button", async () => {
+    expect(screen.getByRole("button", { name: "Update" })).toBeInTheDocument();
+  });
+
+  test("renders show version notes checkbox", async () => {
+    const viewCheckbox = screen.getByText("Show version notes");
+    fireEvent.input(viewCheckbox, {
+      target: {
+        checked: true,
+      },
+    });
+    expect(viewCheckbox).toBeInTheDocument();
+  });
+});
+
+describe("dashboard archived in mobile", () => {
+  beforeEach(() => {
+    // Override the useDashboard mock to return a dashboard in Archived state
+    const archived = { ...dummyDashboard, state: DashboardState.Archived };
+    (hooks.useDashboard as any) = jest.fn().mockReturnValue({
+      loading: false,
+      dashboard: archived,
+    });
+    (hooks.useWindowSize as any) = jest.fn().mockReturnValue({
+      width: 400,
+    });
+    render(<ViewDashboardAdmin />, {
+      wrapper: MemoryRouter,
+    });
+  });
+
+  test("renders a re-publish button", async () => {
+    expect(
+      screen.getByRole("button", { name: "Re-publish" })
+    ).toBeInTheDocument();
+  });
+
+  test("renders a view history button", async () => {
+    expect(
+      screen.getByRole("button", { name: "Archived dashboard actions" })
+    ).toBeInTheDocument();
+  });
+
+  test("renders the re-publish modal", async () => {
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button", { name: "Re-publish" }));
+    });
+
+    const message = await screen.findByText(
+      "Are you sure you want to re-publish this dashboard?"
+    );
+    expect(message).toBeInTheDocument();
+  });
+});
+
+test("renders the update modal", async () => {
+  const published = { ...dummyDashboard, state: DashboardState.Published };
+  (hooks.useDashboard as any) = jest.fn().mockReturnValue({
+    loading: false,
+    dashboard: published,
+  });
+  (hooks.useDashboardVersions as any) = jest.fn().mockReturnValue({
+    loading: false,
+    versions: [
+      { id: "af0dc34f", version: 1, state: "Inactive" },
+      {
+        id: "40db73f9",
+        version: 2,
+        state: "Published",
+        friendlyURL: "hello-world",
+      },
+    ],
+  });
+  (hooks.useWindowSize as any) = jest.fn().mockReturnValue({
+    width: 800,
+  });
+
+  render(<ViewDashboardAdmin />, {
+    wrapper: MemoryRouter,
+  });
+
+  await act(async () => {
+    fireEvent.click(screen.getByRole("button", { name: "Update" }));
+  });
+
+  expect(
+    screen.getByRole("button", { name: "Create draft" })
+  ).toBeInTheDocument();
+});
+
+test("renders the update modal in mobile", async () => {
+  const published = { ...dummyDashboard, state: DashboardState.Published };
+  (hooks.useWindowSize as any) = jest.fn().mockReturnValue({
+    width: 400,
+  });
+  (hooks.useDashboard as any) = jest.fn().mockReturnValue({
+    loading: false,
+    dashboard: published,
+  });
+  (hooks.useDashboardVersions as any) = jest.fn().mockReturnValue({
+    loading: false,
+    versions: [
+      { id: "af0dc34f", version: 1, state: "Inactive" },
+      {
+        id: "40db73f9",
+        version: 2,
+        state: "Published",
+        friendlyURL: "hello-world",
+      },
+    ],
+  });
+
+  render(<ViewDashboardAdmin />, {
+    wrapper: MemoryRouter,
+  });
+
+  await act(async () => {
+    fireEvent.click(screen.getByRole("button", { name: "Update" }));
+  });
+
+  expect(
+    screen.getByRole("button", { name: "Create draft" })
+  ).toBeInTheDocument();
 });

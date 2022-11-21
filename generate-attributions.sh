442901b2030dd8a7d046f01d3b3b8f3d1349e2fb
Miguel Abreu <miabreu@amazon.com>
2022-11-21 18:12:46 -0500

    refactor NOTICE.txt regeneration

--- generate-attributions.sh
+++ generate-attributions.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-npm install -g oss-attribution-generator
+pushd ./notice-generator
 
-cat tools/copyright-notice-header.txt > NOTICE.txt
-dirs="backend cdk e2e-tests examples frontend"
-for d in $dirs; do
-    echo "$d"
-    pushd $d
-    if [ -f ./package.json ]; then
-      echo "checking attributions in $d"
-      generate-attribution
-      #cat ../tools/copyright-notice-header.txt | cat - oss-attribution/attribution.txt > NOTICE
-      echo -e "\n******************************\n" >> ../NOTICE.txt
-      cat oss-attribution/attribution.txt >> ../NOTICE.txt
-      rm -rf oss-attribution
-    fi
-    popd
-done
+# reset file
+cat copyright-notice-header.txt > ../NOTICE.txt
+
+npm run generate
+
+# append new attributions
+cat oss-attribution/attribution.txt >> ../NOTICE.txt
+rm -rf oss-attribution
+
+popd
+

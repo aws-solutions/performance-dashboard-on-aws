c96594e6ccd98d5b6f4fc6747a81bd33a170a099
denisquesada <111308971+denisquesada@users.noreply.github.com>
2022-11-21 12:50:47 -0800

    Unit Tests: frontend/src/hooks. (#953)
    * Unit Tests: frontend/hooks. Part 1
    
    * Unit Tests: frontend/hooks. Part 2

---
+++ frontend/src/hooks/__tests__/image-hooks.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, act, screen } from "@testing-library/react";
+import { useImage } from "../image-hooks";
+import StorageService from "../../services/StorageService";
+
+describe("useImage", () => {
+  interface Props {
+    s3Key: string;
+  }
+  const FooComponent = (props: Props) => {
+    const { file } = useImage(props.s3Key);
+    return (
+      <>
+        <span>{file?.name}</span>
+      </>
+    );
+  };
+
+  test("should fetch the image", async () => {
+    const sampleImage: File = { name: "image.jpg" } as File;
+    const downloadFileSpy = jest
+      .spyOn(StorageService, "downloadFile")
+      .mockReturnValue(Promise.resolve(sampleImage));
+
+    await act(async () => {
+      render(<FooComponent s3Key="/image.jpg" />);
+    });
+
+    expect(downloadFileSpy).toHaveBeenCalled();
+    expect(screen.getByText(sampleImage.name)).toBeInTheDocument();
+  });
+});

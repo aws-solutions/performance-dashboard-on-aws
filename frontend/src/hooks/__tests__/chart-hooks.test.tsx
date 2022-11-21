f4092ffe24ac5e2f23f0c549396c12f263b62d9f
Miguel Abreu <miabreu@amazon.com>
2022-11-21 17:10:36 -0500

    fix existing files licenses

--- frontend/src/hooks/__tests__/chart-hooks.test.tsx
+++ frontend/src/hooks/__tests__/chart-hooks.test.tsx
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import React, { useRef, useState } from "react";
 import { render, act, within } from "@testing-library/react";
 import {
--- frontend/src/hooks/__tests__/dashboard-preview-hooks.test.tsx
+++ frontend/src/hooks/__tests__/dashboard-preview-hooks.test.tsx
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import React from "react";
 import { render, act, fireEvent } from "@testing-library/react";
 import { useFullPreview } from "../dashboard-preview-hooks";

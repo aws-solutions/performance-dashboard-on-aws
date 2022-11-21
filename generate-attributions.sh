9eb8f9c5dfe05a0df160e47828509015804559ff
Miguel Abreu <miabreu@amazon.com>
2022-11-21 17:13:50 -0500

    rename license files

--- generate-attributions.sh
+++ generate-attributions.sh
@@ -2,7 +2,7 @@
 
 npm install -g oss-attribution-generator
 
-cat tools/copyright-notice-header.txt > NOTICE
+cat tools/copyright-notice-header.txt > NOTICE.txt
 dirs="backend cdk e2e-tests examples frontend"
 for d in $dirs; do
     echo "$d"
@@ -11,8 +11,8 @@ for d in $dirs; do
       echo "checking attributions in $d"
       generate-attribution
       #cat ../tools/copyright-notice-header.txt | cat - oss-attribution/attribution.txt > NOTICE
-      echo -e "\n******************************\n" >> ../NOTICE
-      cat oss-attribution/attribution.txt >> ../NOTICE
+      echo -e "\n******************************\n" >> ../NOTICE.txt
+      cat oss-attribution/attribution.txt >> ../NOTICE.txt
       rm -rf oss-attribution
     fi
     popd

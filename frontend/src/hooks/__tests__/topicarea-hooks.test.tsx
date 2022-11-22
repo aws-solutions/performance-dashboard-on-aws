775862330169a0af812d4abb4adfcadb46ca0ddc
Denis Quesada <denisqu@amazon.com>
2022-11-21 18:36:19 -0800

    Unit Tests: frontend/hooks. Part 3

---
+++ frontend/src/hooks/__tests__/topicarea-hooks.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, act, screen } from "@testing-library/react";
+import { useTopicArea } from "../topicarea-hooks";
+import BackendService from "../../services/BackendService";
+
+describe("useTopicArea", () => {
+  interface Props {
+    topicAreaId: string;
+  }
+  const FooComponent = (props: Props) => {
+    const { topicarea } = useTopicArea(props.topicAreaId);
+    return (
+      <>
+        <span>{topicarea?.name}</span>
+      </>
+    );
+  };
+
+  test("should fetch the topic area", async () => {
+    const sampleTopicArea: any = { name: "dummy" };
+    const fetchTopicAreaByIdSpy = jest
+      .spyOn(BackendService, "fetchTopicAreaById")
+      .mockImplementation(() => Promise.resolve(sampleTopicArea));
+
+    await act(async () => {
+      render(<FooComponent topicAreaId="uuid" />);
+    });
+
+    expect(fetchTopicAreaByIdSpy).toHaveBeenCalled();
+    expect(screen.getByText(sampleTopicArea.name)).toBeInTheDocument();
+  });
+});

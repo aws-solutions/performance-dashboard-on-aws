ce4da12f123ac5a76aba7fea6ecde4532f07f7d8
Miguel Abreu <miabreu@amazon.com>
2022-11-22 10:59:40 -0500

    remove THIRD-PARTY-LICENSES generation

--- generate-attributions.sh
+++ generate-attributions.sh
@@ -4,27 +4,17 @@ npm install -g oss-attribution-generator
 npm install -g license-report
 
 cat "" > NOTICE.txt
-cat "" > THIRD-PARTY-LICENSES.txt
 
 dirs="backend cdk examples frontend"
 for d in $dirs; do
     echo "$d"
     pushd $d
     if [ -f ./package.json ]; then
-      echo "checking attributions in $d"
-      generate-attribution
-      echo -e "\n******************************\n" >> ../THIRD-PARTY-LICENSES.txt
-      cat oss-attribution/attribution.txt >> ../THIRD-PARTY-LICENSES.txt
-      rm -rf oss-attribution
-
+      echo "license report for $d"
       license-report --package=package.json --fields=name --fields=licenseType --fields=installedVersion | node ../scripts/parse-dependency.js >> ../NOTICE.txt
     fi
     popd
 done
-
-# remove duplicates
-cat THIRD-PARTY-LICENSES.txt | node scripts/parse-licenses.js > temp
-mv temp THIRD-PARTY-LICENSES.txt
 
 # sort and remove duplicates
 sort -u NOTICE.txt > temp

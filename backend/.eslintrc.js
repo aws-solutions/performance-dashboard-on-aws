2d623777d87977fc9f8a75ef14e4ac65c8f5de7c
Miguel Abreu <mabreuortega@gmail.com>
2022-12-23 12:53:42 -0500

    chore(cdk): enable eslint scan for linted filed (#971)
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -14,11 +14,11 @@ module.exports = {
     },
     rules: {
         "prettier/prettier": ["error"],
-        // "@typescript-eslint/explicit-function-return-type": ["off"],
-        // "@typescript-eslint/strict-boolean-expressions": "off",
-        // "@typescript-eslint/consistent-type-assertions": "off",
-        // "@typescript-eslint/no-floating-promises": "off",
-        // "@typescript-eslint/restrict-template-expressions": "off",
-        // "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/explicit-function-return-type": ["off"],
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/consistent-type-assertions": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/no-misused-promises": "off",
     },
 };

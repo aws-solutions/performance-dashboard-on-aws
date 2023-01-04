e87c2183df7c1afea0a756c348f12c717436cf2b
Miguel Abreu <mabreuortega@gmail.com>
2023-01-04 15:50:43 -0500

    fix(cdk): update http headers name to include frontend id (#973)
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -58,7 +58,7 @@ export class FrontendStack extends Stack {
 
         // Creating a custom response headers policy -- all parameters optional
         const httpHeaders = new ResponseHeadersPolicy(this, "HttpHeaders", {
-            responseHeadersPolicyName: "ReactAppHeaders",
+            responseHeadersPolicyName: `ReactAppHeaders-${id}`,
             securityHeadersBehavior: {
                 // "Content-Security-Policy": "default-src 'self'; img-src 'self' https://*.google-analytics.com blob:; style-src 'unsafe-inline' 'self'; connect-src 'self' https://*.amazoncognito.com https://*.amazonaws.com https://*.google-analytics.com; script-src 'self' https://*.google-analytics.com; block-all-mixed-content;",
                 contentSecurityPolicy: {

e8bd3fd1742a79e0cc6cc3b3bc4d0214b0821906
Miguel Abreu <mabreuortega@gmail.com>
2023-01-10 12:14:43 -0500

    feat(cdk): assign different roles for each cognito role (#979)
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

---
+++ cdk/lib/authz-stack.ts
@@ -0,0 +1,287 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import { UserPool, CfnUserPoolUser, CfnIdentityPoolRoleAttachment } from "aws-cdk-lib/aws-cognito";
+import { Construct } from "constructs";
+import { CfnParameter, Stack, StackProps, CfnOutput } from "aws-cdk-lib";
+import { Effect, FederatedPrincipal, PolicyStatement, Role } from "aws-cdk-lib/aws-iam";
+
+interface Props extends StackProps {
+    datasetsBucketName: string;
+    contentBucketName: string;
+    authenticationRequired: boolean;
+    userPoolArn: string;
+    appClientId: string;
+    identityPoolId: string;
+}
+
+export class AuthorizationStack extends Stack {
+    public readonly adminEmail: string;
+
+    constructor(scope: Construct, id: string, props: Props) {
+        super(scope, id, props);
+
+        /**
+         * CloudFormation parameters
+         */
+        const adminEmail = new CfnParameter(this, "adminEmail", {
+            type: "String",
+            description: "Email address for the admin user",
+            minLength: 5,
+        });
+
+        const pool = UserPool.fromUserPoolArn(this, "UserPool", props.userPoolArn);
+
+        const stack = Stack.of(this);
+        const datasetsBucketArn = `arn:${stack.partition}:s3:::${props.datasetsBucketName}`;
+        const contentBucketArn = `arn:${stack.partition}:s3:::${props.contentBucketName}`;
+
+        const adminRole = this.buildIdentityPoolRole(
+            "CognitoAdminRole",
+            true,
+            props.identityPoolId,
+        );
+        this.addAdminPolicies(adminRole, datasetsBucketArn, contentBucketArn);
+
+        const editorRole = this.buildIdentityPoolRole(
+            "CognitoEditorRole",
+            true,
+            props.identityPoolId,
+        );
+        this.addEditorPolicies(editorRole, datasetsBucketArn, contentBucketArn);
+
+        const protectedRole = this.buildIdentityPoolRole(
+            "CognitoProtectedRole",
+            true,
+            props.identityPoolId,
+        );
+        this.addPublicPolicies(protectedRole, datasetsBucketArn, contentBucketArn);
+
+        const publicRole = this.buildIdentityPoolRole(
+            "CognitoPublicRole",
+            false,
+            props.identityPoolId,
+        );
+        this.addPublicPolicies(publicRole, datasetsBucketArn, contentBucketArn);
+
+        const denyRole = this.buildIdentityPoolRole("CognitoDenyRole", false, props.identityPoolId);
+
+        const providerUrl = `cognito-idp.${stack.region}.amazonaws.com/${pool.userPoolId}:${props.appClientId}`;
+        new CfnIdentityPoolRoleAttachment(this, "AuthRoleAttachment", {
+            identityPoolId: props.identityPoolId,
+            roles: {
+                authenticated: protectedRole.roleArn,
+                unauthenticated: props.authenticationRequired
+                    ? denyRole.roleArn
+                    : publicRole.roleArn,
+            },
+            roleMappings: {
+                cognito: {
+                    ambiguousRoleResolution: "AuthenticatedRole",
+                    type: "Rules",
+                    identityProvider: providerUrl,
+                    rulesConfiguration: {
+                        rules: [
+                            {
+                                claim: "custom:roles",
+                                matchType: "Contains",
+                                value: "Admin",
+                                roleArn: adminRole.roleArn,
+                            },
+                            {
+                                claim: "custom:roles",
+                                matchType: "Contains",
+                                value: "Editor",
+                                roleArn: editorRole.roleArn,
+                            },
+                            {
+                                claim: "custom:roles",
+                                matchType: "Contains",
+                                value: "Public",
+                                roleArn: protectedRole.roleArn,
+                            },
+                        ],
+                    },
+                },
+            },
+        });
+
+        const adminUser = new CfnUserPoolUser(this, "AdminUser", {
+            userPoolId: pool.userPoolId,
+            username: adminEmail.valueAsString,
+            userAttributes: [
+                {
+                    name: "email",
+                    value: adminEmail.valueAsString,
+                },
+                {
+                    name: "custom:roles",
+                    value: JSON.stringify(["Admin"]),
+                },
+            ],
+        });
+
+        /**
+         * Outputs
+         */
+        this.adminEmail = adminEmail.valueAsString;
+
+        new CfnOutput(this, "AdminUsername", { value: adminUser.ref });
+    }
+
+    private addAdminPolicies(adminRole: Role, datasetsBucketArn: string, contentBucketArn: string) {
+        // The following policy gives user's access to the datasets S3 bucket to upload
+        // files. The permissions on this policy are taken from the Amplify docs:
+        // https://docs.amplify.aws/lib/storage/getting-started/q/platform/js#using-amazon-s3
+
+        adminRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:GetObject", "s3:PutObject"],
+                resources: [
+                    datasetsBucketArn.concat("/public/*"),
+                    datasetsBucketArn.concat("/protected/${cognito-identity.amazonaws.com:sub}/*"),
+                    datasetsBucketArn.concat("/private/${cognito-identity.amazonaws.com:sub}/*"),
+
+                    contentBucketArn.concat("/public/*"),
+                    contentBucketArn.concat("/protected/${cognito-identity.amazonaws.com:sub}/*"),
+                    contentBucketArn.concat("/private/${cognito-identity.amazonaws.com:sub}/*"),
+                ],
+            }),
+        );
+
+        adminRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:DeleteObject"],
+                resources: [
+                    contentBucketArn.concat("/public/logo/*"),
+                    contentBucketArn.concat("/public/favicon/*"),
+                ],
+            }),
+        );
+
+        adminRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:PutObject"],
+                resources: [
+                    datasetsBucketArn.concat("/uploads/*"),
+                    contentBucketArn.concat("/uploads/*"),
+                ],
+            }),
+        );
+
+        adminRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:GetObject"],
+                resources: [
+                    datasetsBucketArn.concat("/protected/*"),
+                    contentBucketArn.concat("/protected/*"),
+                ],
+            }),
+        );
+    }
+
+    private addEditorPolicies(
+        editorRole: Role,
+        datasetsBucketArn: string,
+        contentBucketArn: string,
+    ) {
+        // The following policy gives user's access to the datasets S3 bucket to upload
+        // files. The permissions on this policy are taken from the Amplify docs:
+        // https://docs.amplify.aws/lib/storage/getting-started/q/platform/js#using-amazon-s3
+
+        editorRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:GetObject", "s3:PutObject"],
+                resources: [
+                    datasetsBucketArn.concat("/public/*"),
+                    datasetsBucketArn.concat("/protected/${cognito-identity.amazonaws.com:sub}/*"),
+                    datasetsBucketArn.concat("/private/${cognito-identity.amazonaws.com:sub}/*"),
+
+                    contentBucketArn.concat("/public/*"),
+                    contentBucketArn.concat("/protected/${cognito-identity.amazonaws.com:sub}/*"),
+                    contentBucketArn.concat("/private/${cognito-identity.amazonaws.com:sub}/*"),
+                ],
+            }),
+        );
+
+        editorRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:PutObject"],
+                resources: [
+                    datasetsBucketArn.concat("/uploads/*"),
+                    contentBucketArn.concat("/uploads/*"),
+                ],
+            }),
+        );
+
+        editorRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:GetObject"],
+                resources: [
+                    datasetsBucketArn.concat("/protected/*"),
+                    contentBucketArn.concat("/protected/*"),
+                ],
+            }),
+        );
+    }
+
+    private addPublicPolicies(
+        publicRole: Role,
+        datasetsBucketArn: string,
+        contentBucketArn: string,
+    ) {
+        // The public role is assumed by unauthenticated identities. Which means,
+        // any user that lands on the public-facing website that does not have a login.
+        // Need to be careful with what permissions you give to this role. It should be as
+        // restricted as possible.
+
+        publicRole.addToPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:GetObject"],
+                resources: [
+                    datasetsBucketArn.concat("/public/*.json"),
+                    datasetsBucketArn.concat("/public/*.png"),
+                    datasetsBucketArn.concat("/public/*.jpg"),
+                    datasetsBucketArn.concat("/public/*.svg"),
+
+                    contentBucketArn.concat("/public/*.json"),
+                    contentBucketArn.concat("/public/*.png"),
+                    contentBucketArn.concat("/public/*.jpg"),
+                    contentBucketArn.concat("/public/*.svg"),
+                ],
+            }),
+        );
+    }
+
+    private buildIdentityPoolRole(
+        name: string,
+        authenticated: boolean,
+        identityPoolId: string,
+    ): Role {
+        const type = authenticated ? "authenticated" : "unauthenticated";
+        return new Role(this, name, {
+            assumedBy: new FederatedPrincipal(
+                "cognito-identity.amazonaws.com",
+                {
+                    StringEquals: {
+                        "cognito-identity.amazonaws.com:aud": identityPoolId,
+                    },
+                    "ForAnyValue:StringLike": {
+                        "cognito-identity.amazonaws.com:amr": type,
+                    },
+                },
+                "sts:AssumeRoleWithWebIdentity",
+            ),
+        });
+    }
+}

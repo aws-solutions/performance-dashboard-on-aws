627fdf8109acb06ce5dbbd994cef367401206173
denisquesada <111308971+denisquesada@users.noreply.github.com>
2023-02-17 11:31:17 -0800

    test(backend): increased code coverage above 80% (#987)
    * test(backend): increased code coverage above 80%
    
    * test(frontend): increased code coverage inside services folder

--- backend/src/lib/controllers/__tests__/topicarea-ctrl.test.ts
+++ backend/src/lib/controllers/__tests__/topicarea-ctrl.test.ts
@@ -9,6 +9,8 @@ import { User } from "../../models/user";
 import TopicAreaCtrl from "../topicarea-ctrl";
 import TopicAreaRepository from "../../repositories/topicarea-repo";
 
+jest.mock("uuid", () => ({ v4: () => "test-id" }));
+
 jest.mock("../../repositories/topicarea-repo");
 
 const user: User = { userId: "johndoe" };
@@ -21,6 +23,60 @@ const res = {
 
 beforeEach(() => {
     TopicAreaRepository.getInstance = jest.fn().mockReturnValue(repository);
+});
+
+describe("createTopicArea", () => {
+    let req: Request;
+    beforeEach(() => {
+        req = {
+            user,
+            body: {
+                name: "New Name",
+            },
+        } as any as Request;
+    });
+
+    it("returns a 400 error when name is missing", async () => {
+        delete req.body.name;
+        await TopicAreaCtrl.createTopicArea(req, res);
+        expect(res.status).toBeCalledWith(400);
+        expect(res.send).toBeCalledWith("Missing required field `name`");
+        expect(repository.create).not.toBeCalled();
+    });
+
+    it("creates a topic area successfully", async () => {
+        await TopicAreaCtrl.createTopicArea(req, res);
+        expect(repository.create).toBeCalledWith({
+            id: "test-id",
+            name: "New Name",
+            createdBy: user.userId,
+        });
+    });
+});
+
+describe("getTopicAreaById", () => {
+    let req: Request;
+    beforeEach(() => {
+        req = {
+            user,
+            params: {
+                id: "3ffdb1ef-081d-4534-97e9-b69cdbb687d0",
+            },
+        } as any as Request;
+    });
+
+    it("returns a 400 error when id is missing", async () => {
+        delete req.params.id;
+        await TopicAreaCtrl.getTopicAreaById(req, res);
+        expect(res.status).toBeCalledWith(400);
+        expect(res.send).toBeCalledWith("Missing required field `id`");
+        expect(repository.getTopicAreaById).not.toBeCalled();
+    });
+
+    it("get a topic area successfully", async () => {
+        await TopicAreaCtrl.getTopicAreaById(req, res);
+        expect(repository.getTopicAreaById).toBeCalledWith(req.params.id);
+    });
 });
 
 describe("updateTopicArea", () => {

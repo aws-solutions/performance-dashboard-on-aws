c67da7745ed058ceff24c2bb72c041e00eef14a1
denisquesada <111308971+denisquesada@users.noreply.github.com>
2023-02-21 15:45:25 -0800

    test(frontend): hooks code coverage increase (#990)

--- frontend/src/hooks/__tests__/dataset-hooks.test.tsx
+++ frontend/src/hooks/__tests__/dataset-hooks.test.tsx
@@ -9,6 +9,7 @@ import { useDatasets, useJsonDataset, useSampleDataset } from "../dataset-hooks"
 import BackendService from "../../services/BackendService";
 import StorageService from "../../services/StorageService";
 import Papa from "papaparse";
+import { Dataset, DatasetSchema, SourceType } from "../../models";
 
 jest.mock("papaparse");
 
@@ -23,16 +24,39 @@ describe("useDatasets", () => {
     };
 
     test("should fetch datasets", async () => {
+        const datasetSamples: Dataset[] = [
+            {
+                id: "ingest-api",
+                fileName: "ingest-api",
+                s3Key: { raw: "raw", json: "json" },
+                sourceType: SourceType.IngestApi,
+                schema: DatasetSchema.None,
+            },
+            {
+                id: "ingest-api-metrics",
+                fileName: "ingest-api-metrics",
+                s3Key: { raw: "raw", json: "json" },
+                sourceType: SourceType.IngestApi,
+                schema: DatasetSchema.Metrics,
+            },
+            {
+                id: "file-upload",
+                fileName: "file-upload",
+                s3Key: { raw: "raw", json: "json" },
+                sourceType: SourceType.FileUpload,
+                schema: DatasetSchema.None,
+            },
+        ];
         const fetchDatasetsSpy = jest
             .spyOn(BackendService, "fetchDatasets")
-            .mockImplementation(() => Promise.resolve([]));
+            .mockImplementation(() => Promise.resolve(datasetSamples));
 
         await act(async () => {
             render(<FooComponent />);
         });
 
         expect(fetchDatasetsSpy).toHaveBeenCalled();
-        expect(screen.getByText(0)).toBeInTheDocument();
+        expect(screen.getByText(datasetSamples.length)).toBeInTheDocument();
     });
 });
 

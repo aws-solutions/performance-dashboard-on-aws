7b1cc179bc8e157051987409c114f2ab44debd6e
Miguel Abreu <mabreuortega@gmail.com>
2023-03-08 15:16:46 -0500

    Security Enhacenment (#993)
    * fix(cdk): update identity pool role mapping
    
    * fix(cdk): block non https request to s3 buckets
    
    * fix: license check path
    
    ---------
    
    Co-authored-by: Miguel Abreu <miabreu@amazon.com>

--- generate-attributions.sh
+++ generate-attributions.sh
@@ -3,21 +3,22 @@
 echo "" > NOTICE.txt
 
 parserTool="$PWD/scripts/parse-dependency.js"
-dirs="./ backend cdk examples frontend"
+dirs=". backend cdk examples frontend"
+licenseReportPath="$PWD/node_modules/license-report/index.js"
+output="$PWD/NOTICE.txt"
 for d in $dirs; do
     echo "$d"
     pushd $d
     if [ -f ./package.json ]; then
       echo "license report for $d"
-      license-report --package=package.json --fields=name --fields=licenseType --fields=installedVersion | node $parserTool >> ../NOTICE.txt
+      node $licenseReportPath --package=package.json --fields=name --fields=licenseType --fields=installedVersion | node $parserTool >> $output
     fi
     popd
 done
 
 # sort and remove duplicates
-echo "license-report@6.3.0 under MIT" >> NOTICE.txt
-sort -u NOTICE.txt > temp
-mv temp NOTICE.txt
+sort -u $output > temp
+mv temp $output
 
 # prepend license text
-cat scripts/copyright-notice-header.txt | cat - NOTICE.txt > temp && mv temp NOTICE.txt
+cat scripts/copyright-notice-header.txt | cat - $output > temp && mv temp $output
